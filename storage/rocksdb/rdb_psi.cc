coko156/mysql-5.6

Introducing a dedicated thread for handling manual compactions in MyRocks
Summary:
SET GLOBAL rocksdb_compact_cf='cf_name' is a command to trigger
a manual compaction. Manual compaction may take a long time, depending
on target column family size. Prior to this diff, when running manual
compaction with SET GLOBAL, clean mysqld shutdown was blocked until the
manual compaction finished. This was because shutdown path had no way to
terminate a user thread calling CompactRange.

This diff changes how to handle manual compactions. Changes are as follows.

- Introducing a
dedicated MyRocks thread to handle manual compactions (via SET GLOBAL
rocksdb_compact_cf). Inside RocksDB, only one manual compaction can be
running at a time so concurrency does not change.
- Adding a system variable (rocksdb_max_manual_compactions), a debug system variable, and a session variable (rocksdb_manual_compaction_threads), and a few status variables to track manual compaction progress.

With this diff, normal shutdown no longer waits for manual compaction to finish. Also, you can change the number of manual compaction threads based on each compaction. For example, you can increase compaction threads to 16 for large CFs, and uses 1 thread for small CFs.

A user thread calling SET GLOBAL
rocksdb_compact_cf checks the manual compaction thread activities, as
well as its own thd state. If its manual compaction finishes, or its own
thd is killed, the user thread proceeds. As a result, mysqld normal
shutdown is no longer blocked by long running manual compactions.

Manual compaction thread does not persist any state.

This diff does not change OPTIMIZE TABLE behavior. It will be covered by future
diffs.

Test Plan: mtr

Reviewers: herman, mung

Reviewed By: mung

Subscribers: sdong, andrewkr, webscalesql-eng@fb.com

Differential Revision: https://phabricator.intern.facebook.com/D8874346

Signature: 8874346:1532552297:de097801c51f38a25b13c1af4fa51dbf671af306

storage/rocksdb/rdb_psi.cc
@@ -38,22 +38,25 @@ my_core::PSI_stage_info stage_waiting_on_row_lock = {0, "Waiting for row lock",
 my_core::PSI_stage_info *all_rocksdb_stages[] = {&stage_waiting_on_row_lock};
 
 my_core::PSI_thread_key rdb_background_psi_thread_key,
-    rdb_drop_idx_psi_thread_key;
+    rdb_drop_idx_psi_thread_key, rdb_mc_psi_thread_key;
 
 my_core::PSI_thread_info all_rocksdb_threads[] = {
     {&rdb_background_psi_thread_key, "background", PSI_FLAG_GLOBAL},
     {&rdb_drop_idx_psi_thread_key, "drop index", PSI_FLAG_GLOBAL},
+    {&rdb_mc_psi_thread_key, "manual compaction", PSI_FLAG_GLOBAL},
 };
 
 my_core::PSI_mutex_key rdb_psi_open_tbls_mutex_key, rdb_signal_bg_psi_mutex_key,
-    rdb_signal_drop_idx_psi_mutex_key, rdb_collation_data_mutex_key,
-    rdb_mem_cmp_space_mutex_key, key_mutex_tx_list, rdb_sysvars_psi_mutex_key,
-    rdb_cfm_mutex_key, rdb_sst_commit_key, rdb_block_cache_resize_mutex_key;
+    rdb_signal_drop_idx_psi_mutex_key, rdb_signal_mc_psi_mutex_key,
+    rdb_collation_data_mutex_key, rdb_mem_cmp_space_mutex_key,
+    key_mutex_tx_list, rdb_sysvars_psi_mutex_key, rdb_cfm_mutex_key,
+    rdb_sst_commit_key, rdb_block_cache_resize_mutex_key;
 
 my_core::PSI_mutex_info all_rocksdb_mutexes[] = {
     {&rdb_psi_open_tbls_mutex_key, "open tables", PSI_FLAG_GLOBAL},
     {&rdb_signal_bg_psi_mutex_key, "stop background", PSI_FLAG_GLOBAL},
     {&rdb_signal_drop_idx_psi_mutex_key, "signal drop index", PSI_FLAG_GLOBAL},
+    {&rdb_signal_mc_psi_mutex_key, "signal manual compaction", PSI_FLAG_GLOBAL},
     {&rdb_collation_data_mutex_key, "collation data init", PSI_FLAG_GLOBAL},
     {&rdb_mem_cmp_space_mutex_key, "collation space char data init",
      PSI_FLAG_GLOBAL},
@@ -77,11 +80,13 @@ my_core::PSI_rwlock_info all_rocksdb_rwlocks[] = {
 };
 
 my_core::PSI_cond_key rdb_signal_bg_psi_cond_key,
-    rdb_signal_drop_idx_psi_cond_key;
+    rdb_signal_drop_idx_psi_cond_key, rdb_signal_mc_psi_cond_key;
 
 my_core::PSI_cond_info all_rocksdb_conds[] = {
     {&rdb_signal_bg_psi_cond_key, "cond signal background", PSI_FLAG_GLOBAL},
     {&rdb_signal_drop_idx_psi_cond_key, "cond signal drop index",
+     PSI_FLAG_GLOBAL},
+    {&rdb_signal_mc_psi_cond_key, "cond signal manual compaction",
      PSI_FLAG_GLOBAL},
 };
 

mydb-ru/mydb-server

Upstream commit ID : fb-mysql-5.6.35/566b7f095eb1d62a6379c33c911f36d7434898e7
PS-5217 : Merge fb-prod201803
- Introducing a dedicated MyRocks thread to handle manual compactions (via SET
  GLOBAL rocksdb_compact_cf). Inside RocksDB, only one manual compaction can be
  running at a time so concurrency does not change.
- Adding a system variable (rocksdb_max_manual_compactions), a debug system
  variable, and a session variable (rocksdb_manual_compaction_threads), and a
  few status variables to track manual compaction progress.
With this diff, normal shutdown no longer waits for manual compaction to finish.
Also, you can change the number of manual compaction threads based on each
compaction. For example, you can increase compaction threads to 16 for large
CFs, and uses 1 thread for small CFs.
A user thread calling SET GLOBAL rocksdb_compact_cf checks the manual
compaction thread activities, as well as its own thd state. If its manual
compaction finishes, or its own thd is killed, the user thread proceeds. As a
result, mysqld normal shutdown is no longer blocked by long running manual
compactions.
Reviewed By: lth
Differential Revision: D8874346
fbshipit-source-id: 396f787ff78
Percona Server:
- Fixed issue with debug delay variable where code depended on specific expected
  behavior of my_sleep w.r.t. shutdown is different in PS 5.7.
- Moved testing into its own eplicit test file and fixed test to use proper mtr
  idioms.
- Slightly touched up commit comment formatting.

storage/rocksdb/rdb_psi.cc
@@ -35,22 +35,27 @@ my_core::PSI_stage_info stage_waiting_on_row_lock(0, "Waiting for row lock", 0,
 my_core::PSI_stage_info *all_rocksdb_stages[] = {&stage_waiting_on_row_lock};
 
 my_core::PSI_thread_key rdb_background_psi_thread_key,
-    rdb_drop_idx_psi_thread_key;
+    rdb_drop_idx_psi_thread_key, rdb_mc_psi_thread_key;
 
 my_core::PSI_thread_info all_rocksdb_threads[] = {
     {&rdb_background_psi_thread_key, "background", PSI_FLAG_SINGLETON},
     {&rdb_drop_idx_psi_thread_key, "drop index", PSI_FLAG_SINGLETON},
+    {&rdb_mc_psi_thread_key, "manual compaction", PSI_FLAG_SINGLETON},
 };
 
 my_core::PSI_mutex_key rdb_psi_open_tbls_mutex_key, rdb_signal_bg_psi_mutex_key,
-    rdb_signal_drop_idx_psi_mutex_key, rdb_collation_data_mutex_key,
-    rdb_mem_cmp_space_mutex_key, key_mutex_tx_list, rdb_sysvars_psi_mutex_key,
-    rdb_cfm_mutex_key, rdb_sst_commit_key, rdb_block_cache_resize_mutex_key;
+    rdb_signal_drop_idx_psi_mutex_key, rdb_signal_mc_psi_mutex_key,
+    rdb_collation_data_mutex_key, rdb_mem_cmp_space_mutex_key,
+    key_mutex_tx_list, rdb_sysvars_psi_mutex_key, rdb_cfm_mutex_key,
+    rdb_sst_commit_key, rdb_block_cache_resize_mutex_key;
 
 my_core::PSI_mutex_info all_rocksdb_mutexes[] = {
     {&rdb_psi_open_tbls_mutex_key, "open tables", PSI_FLAG_SINGLETON},
     {&rdb_signal_bg_psi_mutex_key, "stop background", PSI_FLAG_SINGLETON},
     {&rdb_signal_drop_idx_psi_mutex_key, "signal drop index",
+     PSI_FLAG_SINGLETON},
+    {&rdb_collation_data_mutex_key, "collation data init", PSI_FLAG_SINGLETON},
+    {&rdb_signal_mc_psi_mutex_key, "signal manual compaction",
      PSI_FLAG_SINGLETON},
     {&rdb_collation_data_mutex_key, "collation data init", PSI_FLAG_SINGLETON},
     {&rdb_mem_cmp_space_mutex_key, "collation space char data init",
@@ -76,11 +81,13 @@ my_core::PSI_rwlock_info all_rocksdb_rwlocks[] = {
 };
 
 my_core::PSI_cond_key rdb_signal_bg_psi_cond_key,
-    rdb_signal_drop_idx_psi_cond_key;
+    rdb_signal_drop_idx_psi_cond_key, rdb_signal_mc_psi_cond_key;
 
 my_core::PSI_cond_info all_rocksdb_conds[] = {
     {&rdb_signal_bg_psi_cond_key, "cond signal background", PSI_FLAG_SINGLETON},
     {&rdb_signal_drop_idx_psi_cond_key, "cond signal drop index",
+     PSI_FLAG_SINGLETON},
+    {&rdb_signal_mc_psi_cond_key, "cond signal manual compaction",
      PSI_FLAG_SINGLETON},
 };
 