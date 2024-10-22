percona/percona-server

PS-8422: Merge MySQL 8.0.31 (valgrind "Invalid read of size 8" in rpl.rpl_parallel_multi_db)
https://jira.percona.com/browse/PS-8422

The "Invalid read of size 8" Valgrind error is observed in
rpl.rpl_parallel_multi_db MTR test results.

The https://github.com/percona/percona-server/commit/bdd12ac67a6cb0e1e295457205e9f640c6e9d0ab
change introduced check to prevent dangerous use of rpl writebatch for
rocksdb. In order to find out if the feature is used properly in
Query_log_event::do_apply_event() we check if handlers for all query
tables support the feature. The issue here is that by the time the check
is performed all involved table objects are already released for later
reuse or complettely closed and memory is freed.

To fix the issue corresponding check was changed to be done at
the point where all query table have just been opened.

sql/log_event.cc
@@ -4909,29 +4909,19 @@ int Query_log_event::do_apply_event(Relay_log_info const *rli,
         }
 
         if (sqlcom_can_generate_row_events(thd->lex->sql_command) &&
-            thd->get_row_count_func() > 0) {
-          for (TABLE_LIST *tbl = thd->lex->query_tables; tbl;
-               tbl = tbl->next_global) {
-            if (!tbl->is_placeholder() && tbl->table->file) {
-              if (!tbl->table->file->rpl_can_handle_stm_event()) {
-                String message;
-                message.append(
-                    "Masters binlog format is not ROW and storage "
-                    "engine can not handle non-ROW events at this "
-                    "time. Table: '");
-                message.append(tbl->get_db_name());
-                message.append(".");
-                message.append(tbl->get_table_name());
-                message.append("' Query: '");
-                message.append(thd->query().str);
-                message.append("'");
-                rli->report(ERROR_LEVEL, ER_SLAVE_FATAL_ERROR,
-                            ER_THD(thd, ER_SLAVE_FATAL_ERROR), message.c_ptr());
-                thd->is_slave_error = true;
-                goto end;
-              }
-            }
-          }
+            thd->get_row_count_func() > 0 &&
+            !thd->get_rpl_stmt_event_format_used()) {
+          String message;
+          message.append(
+              "Masters binlog format is not ROW and storage "
+              "engine can not handle non-ROW events at this "
+              "time. Query: '");
+          message.append(thd->query().str);
+          message.append("'");
+          rli->report(ERROR_LEVEL, ER_SLAVE_FATAL_ERROR,
+                      ER_THD(thd, ER_SLAVE_FATAL_ERROR), message.c_ptr());
+          thd->is_slave_error = true;
+          goto end;
         }
 
         /*

mydb-ru/mydb-server

sql/log_event.cc
@@ -4841,29 +4841,19 @@ int Query_log_event::do_apply_event(Relay_log_info const *rli,
         }
 
         if (sqlcom_can_generate_row_events(thd->lex->sql_command) &&
-            thd->get_row_count_func() > 0) {
-          for (Table_ref *tbl = thd->lex->query_tables; tbl;
-               tbl = tbl->next_global) {
-            if (!tbl->is_placeholder() && tbl->table->file) {
-              if (!tbl->table->file->rpl_can_handle_stm_event()) {
-                String message;
-                message.append(
-                    "Masters binlog format is not ROW and storage "
-                    "engine can not handle non-ROW events at this "
-                    "time. Table: '");
-                message.append(tbl->get_db_name());
-                message.append(".");
-                message.append(tbl->get_table_name());
-                message.append("' Query: '");
-                message.append(thd->query().str);
-                message.append("'");
-                rli->report(ERROR_LEVEL, ER_REPLICA_FATAL_ERROR,
-                            ER_THD(thd, ER_REPLICA_FATAL_ERROR), message.c_ptr());
-                thd->is_slave_error = true;
-                goto end;
-              }
-            }
-          }
+            thd->get_row_count_func() > 0 &&
+            !thd->get_rpl_stmt_event_format_used()) {
+          String message;
+          message.append(
+              "Masters binlog format is not ROW and storage "
+              "engine can not handle non-ROW events at this "
+              "time. Query: '");
+          message.append(thd->query().str);
+          message.append("'");
+          rli->report(ERROR_LEVEL, ER_REPLICA_FATAL_ERROR,
+                      ER_THD(thd, ER_REPLICA_FATAL_ERROR), message.c_ptr());
+          thd->is_slave_error = true;
+          goto end;
         }
 
         /*