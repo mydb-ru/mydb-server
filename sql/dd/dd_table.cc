inikep/percona-server

PS-4439: Port clustering secondary index patch to 8.0
Summary:
--------
Support CLUSTERING KEY type with new Data Dictionary

A new options property "clustering_key" is used to determine the
CLUSTERING KEY type. This is stored as part of dd::Index::options

KEY* flag HA_CLUSTERING is set based on clustering key property of
dd::Index::options (fill_index_from_dd())

Tests which are failing due to collation changes, "row" count
mismatch of explain output (wrong column masked) are rerecorded.

sql/dd/dd_table.cc
@@ -1052,6 +1052,10 @@ static void fill_dd_indexes_from_keyinfo(
     if (key->parser_name.str)
       idx_options->set("parser_name", key->parser_name.str);
 
+    if (key->flags & HA_CLUSTERING) {
+      idx_options->set_bool("clustering_key", true);
+    }
+
     /*
       If we have no primary key, then we pick the first candidate primary
       key and promote it. When we promote, the field's of key_part needs to

sql/dd_table_share.cc
@@ -1187,6 +1187,16 @@ static bool fill_index_from_dd(THD *thd, TABLE_SHARE *share,
       break;
   }
 
+  /* Check if this index is of clustering key type. Used by TokuDB */
+  const dd::Properties &index_options = idx_obj->options();
+  bool has_clustering = false;
+  if (index_options.exists("clustering_key")) {
+    index_options.get_bool("clustering_key", &has_clustering);
+  }
+  if (has_clustering) {
+    keyinfo->flags |= HA_CLUSTERING;
+  }
+
   if (idx_obj->is_generated()) keyinfo->flags |= HA_GENERATED_KEY;
 
   /*

mydb-ru/mydb-server

sql/dd/dd_table.cc
@@ -1115,6 +1115,10 @@ static void fill_dd_indexes_from_keyinfo(
     if (key->parser_name.str)
       idx_options->set("parser_name", key->parser_name.str);
 
+    if (key->flags & HA_CLUSTERING) {
+      idx_options->set("clustering_key", true);
+    }
+
     /*
       If we have no primary key, then we pick the first candidate primary
       key and promote it. When we promote, the field's of key_part needs to

sql/dd_table_share.cc
@@ -1371,6 +1371,16 @@ static bool fill_index_from_dd(THD *thd, TABLE_SHARE *share,
       break;
   }
 
+  /* Check if this index is of clustering key type. Used by TokuDB */
+  const dd::Properties &index_options = idx_obj->options();
+  bool has_clustering = false;
+  if (index_options.exists("clustering_key")) {
+    index_options.get("clustering_key", &has_clustering);
+  }
+  if (has_clustering) {
+    keyinfo->flags |= HA_CLUSTERING;
+  }
+
   if (idx_obj->is_generated()) keyinfo->flags |= HA_GENERATED_KEY;
 
   /*