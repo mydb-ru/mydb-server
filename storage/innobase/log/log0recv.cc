inikep/percona-server

[encryption] PS-5333: Remove need for innodb_encrypt_tables to ON/FORCE when mysql.ibd is encrypted
PS-5117 introduced requirement that innodb_encrypt_tables need to be set
to ON/FORCE on server startup when mysql.ibd is encrypted. This
requirement was added because there was a need for mysql.ibd space flags
validation against hardcoded, predefined flags. In PS mysql.ibd can be
encrypted and have encrypted flag set - thus generating a mismatch with
predefined flags. In order to check whether mysql.ibd is encrypted we
will check flags from the first page of the tablespace instead of
relaying on innodb_encrypt_tables. Apart from improvement from user
point of view it will also ease keyring encryption threads development
in 8.0 as encryption threads can also change the mysql.ibd's encryption
flag.

In keyring encryption there is a possibility that update to mysql.ibd's
page0 will reside only inside redo log (due to the crash).
Added code for reading page0 encryption flag from redo log and using it
to update mysql.ibd's DD flag. This code will be tested when keyring
encryption gets fixed.

storage/innobase/log/log0recv.cc
@@ -735,6 +735,10 @@ static recv_addr_t *recv_get_rec(space_id_t space_id, page_no_t page_no) {
   return (nullptr);
 }
 
+bool is_mysql_ibd_page_0_in_redo() {
+  return recv_get_rec(dict_sys_t::s_space_id, 0) != nullptr;
+}
+
 #ifndef UNIV_HOTBACKUP
 /** Store the collected persistent dynamic metadata to
 mysql.innodb_dynamic_metadata */

mydb-ru/mydb-server

storage/innobase/log/log0recv.cc
@@ -663,6 +663,10 @@ static bool log_block_epoch_no_is_valid(uint32_t log_block_epoch_no,
          log_block_epoch_no == expected_next_epoch_no;
 }
 
+bool is_mysql_ibd_page_0_in_redo() {
+  return recv_get_rec(dict_sys_t::s_dict_space_id, 0) != nullptr;
+}
+
 #ifndef UNIV_HOTBACKUP
 /** Store the collected persistent dynamic metadata to
 mysql.innodb_dynamic_metadata */