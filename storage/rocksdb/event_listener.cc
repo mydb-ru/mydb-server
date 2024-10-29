inikep/percona-server

[storage/rocksdb] PS-269: Initial Percona Server 8.0.12 tree

storage/rocksdb/event_listener.cc
@@ -1,0 +1,93 @@
+/*
+   Copyright (c) 2015, Facebook, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; version 2 of the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
+
+/* The C++ file's header */
+#include "./event_listener.h"
+
+/* C++ standard header files */
+#include <string>
+#include <vector>
+
+/* MySQL includes */
+#include "mysql/plugin.h"
+
+/* MyRocks includes */
+#include "./ha_rocksdb.h"
+#include "./properties_collector.h"
+#include "./rdb_datadic.h"
+
+namespace myrocks {
+
+static std::vector<Rdb_index_stats>
+extract_index_stats(const std::vector<std::string> &files,
+                    const rocksdb::TablePropertiesCollection &props) {
+  std::vector<Rdb_index_stats> ret;
+  for (auto fn : files) {
+    const auto it = props.find(fn);
+    DBUG_ASSERT(it != props.end());
+    std::vector<Rdb_index_stats> stats;
+    Rdb_tbl_prop_coll::read_stats_from_tbl_props(it->second, &stats);
+    ret.insert(ret.end(), stats.begin(), stats.end());
+  }
+  return ret;
+}
+
+void Rdb_event_listener::update_index_stats(
+    const rocksdb::TableProperties &props) {
+  DBUG_ASSERT(m_ddl_manager != nullptr);
+  const auto tbl_props =
+      std::make_shared<const rocksdb::TableProperties>(props);
+
+  std::vector<Rdb_index_stats> stats;
+  Rdb_tbl_prop_coll::read_stats_from_tbl_props(tbl_props, &stats);
+
+  m_ddl_manager->adjust_stats(stats);
+}
+
+void Rdb_event_listener::OnCompactionCompleted(
+    rocksdb::DB *db, const rocksdb::CompactionJobInfo &ci) {
+  DBUG_ASSERT(db != nullptr);
+  DBUG_ASSERT(m_ddl_manager != nullptr);
+
+  if (ci.status.ok()) {
+    m_ddl_manager->adjust_stats(
+        extract_index_stats(ci.output_files, ci.table_properties),
+        extract_index_stats(ci.input_files, ci.table_properties));
+  }
+}
+
+void Rdb_event_listener::OnFlushCompleted(
+    rocksdb::DB *db, const rocksdb::FlushJobInfo &flush_job_info) {
+  DBUG_ASSERT(db != nullptr);
+  update_index_stats(flush_job_info.table_properties);
+}
+
+void Rdb_event_listener::OnExternalFileIngested(
+    rocksdb::DB *db, const rocksdb::ExternalFileIngestionInfo &info) {
+  DBUG_ASSERT(db != nullptr);
+  update_index_stats(info.table_properties);
+}
+
+void Rdb_event_listener::OnBackgroundError(
+    rocksdb::BackgroundErrorReason reason, rocksdb::Status *status) {
+  rdb_log_status_error(*status, "Error detected in background");
+  LogPluginErrMsg(ERROR_LEVEL, 0, "BackgroundErrorReason: %d", (int)reason);
+  if (status->IsCorruption()) {
+    rdb_persist_corruption_marker();
+    abort();
+  }
+}
+}  // namespace myrocks

storage/rocksdb/rdb_cf_manager.cc
@@ -1,0 +1,190 @@
+/*
+   Copyright (c) 2014, SkySQL Ab
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; version 2 of the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
+
+#ifdef USE_PRAGMA_IMPLEMENTATION
+#pragma implementation  // gcc: Class implementation
+#endif
+
+/* This C++ files header file */
+#include "./rdb_cf_manager.h"
+
+/* MyRocks header files */
+#include "./ha_rocksdb.h"
+#include "./ha_rocksdb_proto.h"
+#include "./rdb_psi.h"
+
+namespace myrocks {
+
+/* Check if ColumnFamily name says it's a reverse-ordered CF */
+bool Rdb_cf_manager::is_cf_name_reverse(const char *const name) {
+  /* nullptr means the default CF is used.. (TODO: can the default CF be
+   * reverse?) */
+  return (name && !strncmp(name, "rev:", 4));
+}
+
+void Rdb_cf_manager::init(
+    std::unique_ptr<Rdb_cf_options> cf_options,
+    std::vector<rocksdb::ColumnFamilyHandle *> *const handles) {
+#ifdef HAVE_PSI_INTERFACE
+  mysql_mutex_init(rdb_cfm_mutex_key, &m_mutex, MY_MUTEX_INIT_FAST);
+#else
+  mysql_mutex_init(&m_mutex, MY_MUTEX_INIT_FAST);
+#endif
+
+  DBUG_ASSERT(cf_options != nullptr);
+  DBUG_ASSERT(handles != nullptr);
+  DBUG_ASSERT(handles->size() > 0);
+
+  m_cf_options = std::move(cf_options);
+
+  for (auto cfh : *handles) {
+    DBUG_ASSERT(cfh != nullptr);
+    m_cf_name_map[cfh->GetName()] = cfh;
+    m_cf_id_map[cfh->GetID()] = cfh;
+  }
+}
+
+void Rdb_cf_manager::cleanup() {
+  for (auto it : m_cf_name_map) {
+    delete it.second;
+  }
+  mysql_mutex_destroy(&m_mutex);
+  m_cf_options = nullptr;
+}
+
+/*
+  @brief
+  Find column family by name. If it doesn't exist, create it
+
+  @detail
+    See Rdb_cf_manager::get_cf
+*/
+rocksdb::ColumnFamilyHandle *
+Rdb_cf_manager::get_or_create_cf(rocksdb::DB *const rdb,
+                                 const std::string &cf_name_arg) {
+  DBUG_ASSERT(rdb != nullptr);
+
+  rocksdb::ColumnFamilyHandle *cf_handle = nullptr;
+
+  if (cf_name_arg == PER_INDEX_CF_NAME) {
+    // per-index column families is no longer supported.
+    my_error(ER_PER_INDEX_CF_DEPRECATED, MYF(0));
+    return nullptr;
+  }
+
+  const std::string &cf_name =
+      cf_name_arg.empty() ? DEFAULT_CF_NAME : cf_name_arg;
+
+  RDB_MUTEX_LOCK_CHECK(m_mutex);
+
+  const auto it = m_cf_name_map.find(cf_name);
+
+  if (it != m_cf_name_map.end()) {
+    cf_handle = it->second;
+  } else {
+    /* Create a Column Family. */
+    rocksdb::ColumnFamilyOptions opts;
+    m_cf_options->get_cf_options(cf_name, &opts);
+
+    LogPluginErrMsg(INFORMATION_LEVEL, 0, "Creating a column family %s",
+                    cf_name.c_str());
+    LogPluginErrMsg(INFORMATION_LEVEL, 0, "    write_buffer_size=%ld",
+                    opts.write_buffer_size);
+    LogPluginErrMsg(INFORMATION_LEVEL, 0, "    target_file_size_base=%" PRIu64,
+                    opts.target_file_size_base);
+
+    const rocksdb::Status s =
+        rdb->CreateColumnFamily(opts, cf_name, &cf_handle);
+
+    if (s.ok()) {
+      m_cf_name_map[cf_handle->GetName()] = cf_handle;
+      m_cf_id_map[cf_handle->GetID()] = cf_handle;
+    } else {
+      cf_handle = nullptr;
+    }
+  }
+
+  RDB_MUTEX_UNLOCK_CHECK(m_mutex);
+
+  return cf_handle;
+}
+
+/*
+  Find column family by its cf_name.
+*/
+
+rocksdb::ColumnFamilyHandle *
+Rdb_cf_manager::get_cf(const std::string &cf_name_arg) const {
+  rocksdb::ColumnFamilyHandle *cf_handle;
+
+  RDB_MUTEX_LOCK_CHECK(m_mutex);
+
+  std::string cf_name = cf_name_arg.empty() ? DEFAULT_CF_NAME : cf_name_arg;
+
+  const auto it = m_cf_name_map.find(cf_name);
+  cf_handle = (it != m_cf_name_map.end()) ? it->second : nullptr;
+
+  if (!cf_handle) {
+    LogPluginErrMsg(WARNING_LEVEL, 0, "Column family '%s' not found.",
+                    cf_name.c_str());
+  }
+
+  RDB_MUTEX_UNLOCK_CHECK(m_mutex);
+
+  return cf_handle;
+}
+
+rocksdb::ColumnFamilyHandle *Rdb_cf_manager::get_cf(const uint32_t &id) const {
+  rocksdb::ColumnFamilyHandle *cf_handle = nullptr;
+
+  RDB_MUTEX_LOCK_CHECK(m_mutex);
+  const auto it = m_cf_id_map.find(id);
+  if (it != m_cf_id_map.end())
+    cf_handle = it->second;
+  RDB_MUTEX_UNLOCK_CHECK(m_mutex);
+
+  return cf_handle;
+}
+
+std::vector<std::string> Rdb_cf_manager::get_cf_names(void) const {
+  std::vector<std::string> names;
+
+  RDB_MUTEX_LOCK_CHECK(m_mutex);
+  for (auto it : m_cf_name_map) {
+    names.push_back(it.first);
+  }
+  RDB_MUTEX_UNLOCK_CHECK(m_mutex);
+
+  return names;
+}
+
+std::vector<rocksdb::ColumnFamilyHandle *>
+Rdb_cf_manager::get_all_cf(void) const {
+  std::vector<rocksdb::ColumnFamilyHandle *> list;
+
+  RDB_MUTEX_LOCK_CHECK(m_mutex);
+
+  for (auto it : m_cf_id_map) {
+    DBUG_ASSERT(it.second != nullptr);
+    list.push_back(it.second);
+  }
+
+  RDB_MUTEX_UNLOCK_CHECK(m_mutex);
+
+  return list;
+}
+
+}  // namespace myrocks

storage/rocksdb/rdb_perf_context.h
@@ -1,0 +1,151 @@
+/*
+   Portions Copyright (c) 2015-Present, Facebook, Inc.
+   Portions Copyright (c) 2012,2013 Monty Program Ab
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; version 2 of the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
+#pragma once
+
+/* C++ standard header files */
+#include <atomic>
+#include <cstdint>
+#include <string>
+
+/* MySQL header files */
+#include "my_io_perf.h"
+#include "sql/handler.h"
+
+namespace myrocks {
+
+enum {
+  PC_USER_KEY_COMPARISON_COUNT = 0,
+  PC_BLOCK_CACHE_HIT_COUNT,
+  PC_BLOCK_READ_COUNT,
+  PC_BLOCK_READ_BYTE,
+  PC_BLOCK_READ_TIME,
+  PC_BLOCK_CHECKSUM_TIME,
+  PC_BLOCK_DECOMPRESS_TIME,
+  PC_GET_READ_BYTES,
+  PC_MULTIGET_READ_BYTES,
+  PC_ITER_READ_BYTES,
+  PC_KEY_SKIPPED,
+  PC_DELETE_SKIPPED,
+  PC_RECENT_SKIPPED,
+  PC_MERGE,
+  PC_GET_SNAPSHOT_TIME,
+  PC_GET_FROM_MEMTABLE_TIME,
+  PC_GET_FROM_MEMTABLE_COUNT,
+  PC_GET_POST_PROCESS_TIME,
+  PC_GET_FROM_OUTPUT_FILES_TIME,
+  PC_SEEK_ON_MEMTABLE_TIME,
+  PC_SEEK_ON_MEMTABLE_COUNT,
+  PC_NEXT_ON_MEMTABLE_COUNT,
+  PC_PREV_ON_MEMTABLE_COUNT,
+  PC_SEEK_CHILD_SEEK_TIME,
+  PC_SEEK_CHILD_SEEK_COUNT,
+  PC_SEEK_MIN_HEAP_TIME,
+  PC_SEEK_MAX_HEAP_TIME,
+  PC_SEEK_INTERNAL_SEEK_TIME,
+  PC_FIND_NEXT_USER_ENTRY_TIME,
+  PC_WRITE_WAL_TIME,
+  PC_WRITE_MEMTABLE_TIME,
+  PC_WRITE_DELAY_TIME,
+  PC_WRITE_PRE_AND_POST_PROCESSS_TIME,
+  PC_DB_MUTEX_LOCK_NANOS,
+  PC_DB_CONDITION_WAIT_NANOS,
+  PC_MERGE_OPERATOR_TIME_NANOS,
+  PC_READ_INDEX_BLOCK_NANOS,
+  PC_READ_FILTER_BLOCK_NANOS,
+  PC_NEW_TABLE_BLOCK_ITER_NANOS,
+  PC_NEW_TABLE_ITERATOR_NANOS,
+  PC_BLOCK_SEEK_NANOS,
+  PC_FIND_TABLE_NANOS,
+  PC_BLOOM_MEMTABLE_HIT_COUNT,
+  PC_BLOOM_MEMTABLE_MISS_COUNT,
+  PC_BLOOM_SST_HIT_COUNT,
+  PC_BLOOM_SST_MISS_COUNT,
+  PC_KEY_LOCK_WAIT_TIME,
+  PC_KEY_LOCK_WAIT_COUNT,
+  PC_IO_THREAD_POOL_ID,
+  PC_IO_BYTES_WRITTEN,
+  PC_IO_BYTES_READ,
+  PC_IO_OPEN_NANOS,
+  PC_IO_ALLOCATE_NANOS,
+  PC_IO_WRITE_NANOS,
+  PC_IO_READ_NANOS,
+  PC_IO_RANGE_SYNC_NANOS,
+  PC_IO_LOGGER_NANOS,
+  PC_MAX_IDX
+};
+
+class Rdb_perf_counters;
+
+/*
+  A collection of performance counters that can be safely incremented by
+  multiple threads since it stores atomic datapoints.
+*/
+struct Rdb_atomic_perf_counters {
+  std::atomic_ullong m_value[PC_MAX_IDX];
+};
+
+/*
+  A collection of performance counters that is meant to be incremented by
+  a single thread.
+*/
+class Rdb_perf_counters {
+  Rdb_perf_counters(const Rdb_perf_counters &) = delete;
+  Rdb_perf_counters &operator=(const Rdb_perf_counters &) = delete;
+
+ public:
+  Rdb_perf_counters() = default;
+  uint64_t m_value[PC_MAX_IDX];
+
+  void load(const Rdb_atomic_perf_counters &atomic_counters);
+};
+
+extern std::string rdb_pc_stat_types[PC_MAX_IDX];
+
+/*
+  Perf timers for data reads
+ */
+class Rdb_io_perf {
+  // Context management
+  Rdb_atomic_perf_counters *m_atomic_counters = nullptr;
+  my_io_perf_atomic_t *m_shared_io_perf_read = nullptr;
+  ha_statistics *m_stats = nullptr;
+
+ public:
+  Rdb_io_perf(const Rdb_io_perf &) = delete;
+  Rdb_io_perf &operator=(const Rdb_io_perf &) = delete;
+
+  void init(Rdb_atomic_perf_counters *const atomic_counters,
+            my_io_perf_atomic_t *const shared_io_perf_read,
+            ha_statistics *const stats) {
+    DBUG_ASSERT(atomic_counters != nullptr);
+    DBUG_ASSERT(shared_io_perf_read != nullptr);
+    DBUG_ASSERT(stats != nullptr);
+
+    m_atomic_counters = atomic_counters;
+    m_shared_io_perf_read = shared_io_perf_read;
+    m_stats = stats;
+  }
+
+  bool start(const uint32_t perf_context_level);
+  void end_and_record(const uint32_t perf_context_level);
+
+  explicit Rdb_io_perf()
+      : m_atomic_counters(nullptr), m_shared_io_perf_read(nullptr),
+        m_stats(nullptr) {}
+};
+
+}  // namespace myrocks

mydb-ru/mydb-server

storage/rocksdb/event_listener.cc
@@ -1,0 +1,93 @@
+/*
+   Copyright (c) 2015, Facebook, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; version 2 of the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
+
+/* The C++ file's header */
+#include "./event_listener.h"
+
+/* C++ standard header files */
+#include <string>
+#include <vector>
+
+/* MySQL includes */
+#include "mysql/plugin.h"
+
+/* MyRocks includes */
+#include "./ha_rocksdb.h"
+#include "./properties_collector.h"
+#include "./rdb_datadic.h"
+
+namespace myrocks {
+
+static std::vector<Rdb_index_stats>
+extract_index_stats(const std::vector<std::string> &files,
+                    const rocksdb::TablePropertiesCollection &props) {
+  std::vector<Rdb_index_stats> ret;
+  for (auto fn : files) {
+    const auto it = props.find(fn);
+    assert(it != props.end());
+    std::vector<Rdb_index_stats> stats;
+    Rdb_tbl_prop_coll::read_stats_from_tbl_props(it->second, &stats);
+    ret.insert(ret.end(), stats.begin(), stats.end());
+  }
+  return ret;
+}
+
+void Rdb_event_listener::update_index_stats(
+    const rocksdb::TableProperties &props) {
+  assert(m_ddl_manager != nullptr);
+  const auto tbl_props =
+      std::make_shared<const rocksdb::TableProperties>(props);
+
+  std::vector<Rdb_index_stats> stats;
+  Rdb_tbl_prop_coll::read_stats_from_tbl_props(tbl_props, &stats);
+
+  m_ddl_manager->adjust_stats(stats);
+}
+
+void Rdb_event_listener::OnCompactionCompleted(
+    rocksdb::DB *db, const rocksdb::CompactionJobInfo &ci) {
+  assert(db != nullptr);
+  assert(m_ddl_manager != nullptr);
+
+  if (ci.status.ok()) {
+    m_ddl_manager->adjust_stats(
+        extract_index_stats(ci.output_files, ci.table_properties),
+        extract_index_stats(ci.input_files, ci.table_properties));
+  }
+}
+
+void Rdb_event_listener::OnFlushCompleted(
+    rocksdb::DB *db, const rocksdb::FlushJobInfo &flush_job_info) {
+  assert(db != nullptr);
+  update_index_stats(flush_job_info.table_properties);
+}
+
+void Rdb_event_listener::OnExternalFileIngested(
+    rocksdb::DB *db, const rocksdb::ExternalFileIngestionInfo &info) {
+  assert(db != nullptr);
+  update_index_stats(info.table_properties);
+}
+
+void Rdb_event_listener::OnBackgroundError(
+    rocksdb::BackgroundErrorReason reason, rocksdb::Status *status) {
+  rdb_log_status_error(*status, "Error detected in background");
+  LogPluginErrMsg(ERROR_LEVEL, 0, "BackgroundErrorReason: %d", (int)reason);
+  if (status->IsCorruption()) {
+    rdb_persist_corruption_marker();
+    abort();
+  }
+}
+}  // namespace myrocks

storage/rocksdb/rdb_cf_manager.cc
@@ -1,0 +1,190 @@
+/*
+   Copyright (c) 2014, SkySQL Ab
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; version 2 of the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
+
+#ifdef USE_PRAGMA_IMPLEMENTATION
+#pragma implementation  // gcc: Class implementation
+#endif
+
+/* This C++ files header file */
+#include "./rdb_cf_manager.h"
+
+/* MyRocks header files */
+#include "./ha_rocksdb.h"
+#include "./ha_rocksdb_proto.h"
+#include "./rdb_psi.h"
+
+namespace myrocks {
+
+/* Check if ColumnFamily name says it's a reverse-ordered CF */
+bool Rdb_cf_manager::is_cf_name_reverse(const char *const name) {
+  /* nullptr means the default CF is used.. (TODO: can the default CF be
+   * reverse?) */
+  return (name && !strncmp(name, "rev:", 4));
+}
+
+void Rdb_cf_manager::init(
+    std::unique_ptr<Rdb_cf_options> cf_options,
+    std::vector<rocksdb::ColumnFamilyHandle *> *const handles) {
+#ifdef HAVE_PSI_INTERFACE
+  mysql_mutex_init(rdb_cfm_mutex_key, &m_mutex, MY_MUTEX_INIT_FAST);
+#else
+  mysql_mutex_init(&m_mutex, MY_MUTEX_INIT_FAST);
+#endif
+
+  assert(cf_options != nullptr);
+  assert(handles != nullptr);
+  assert(handles->size() > 0);
+
+  m_cf_options = std::move(cf_options);
+
+  for (auto cfh : *handles) {
+    assert(cfh != nullptr);
+    m_cf_name_map[cfh->GetName()] = cfh;
+    m_cf_id_map[cfh->GetID()] = cfh;
+  }
+}
+
+void Rdb_cf_manager::cleanup() {
+  for (auto it : m_cf_name_map) {
+    delete it.second;
+  }
+  mysql_mutex_destroy(&m_mutex);
+  m_cf_options = nullptr;
+}
+
+/*
+  @brief
+  Find column family by name. If it doesn't exist, create it
+
+  @detail
+    See Rdb_cf_manager::get_cf
+*/
+rocksdb::ColumnFamilyHandle *
+Rdb_cf_manager::get_or_create_cf(rocksdb::DB *const rdb,
+                                 const std::string &cf_name_arg) {
+  assert(rdb != nullptr);
+
+  rocksdb::ColumnFamilyHandle *cf_handle = nullptr;
+
+  if (cf_name_arg == PER_INDEX_CF_NAME) {
+    // per-index column families is no longer supported.
+    my_error(ER_PER_INDEX_CF_DEPRECATED, MYF(0));
+    return nullptr;
+  }
+
+  const std::string &cf_name =
+      cf_name_arg.empty() ? DEFAULT_CF_NAME : cf_name_arg;
+
+  RDB_MUTEX_LOCK_CHECK(m_mutex);
+
+  const auto it = m_cf_name_map.find(cf_name);
+
+  if (it != m_cf_name_map.end()) {
+    cf_handle = it->second;
+  } else {
+    /* Create a Column Family. */
+    rocksdb::ColumnFamilyOptions opts;
+    m_cf_options->get_cf_options(cf_name, &opts);
+
+    LogPluginErrMsg(INFORMATION_LEVEL, 0, "Creating a column family %s",
+                    cf_name.c_str());
+    LogPluginErrMsg(INFORMATION_LEVEL, 0, "    write_buffer_size=%ld",
+                    opts.write_buffer_size);
+    LogPluginErrMsg(INFORMATION_LEVEL, 0, "    target_file_size_base=%" PRIu64,
+                    opts.target_file_size_base);
+
+    const rocksdb::Status s =
+        rdb->CreateColumnFamily(opts, cf_name, &cf_handle);
+
+    if (s.ok()) {
+      m_cf_name_map[cf_handle->GetName()] = cf_handle;
+      m_cf_id_map[cf_handle->GetID()] = cf_handle;
+    } else {
+      cf_handle = nullptr;
+    }
+  }
+
+  RDB_MUTEX_UNLOCK_CHECK(m_mutex);
+
+  return cf_handle;
+}
+
+/*
+  Find column family by its cf_name.
+*/
+
+rocksdb::ColumnFamilyHandle *
+Rdb_cf_manager::get_cf(const std::string &cf_name_arg) const {
+  rocksdb::ColumnFamilyHandle *cf_handle;
+
+  RDB_MUTEX_LOCK_CHECK(m_mutex);
+
+  std::string cf_name = cf_name_arg.empty() ? DEFAULT_CF_NAME : cf_name_arg;
+
+  const auto it = m_cf_name_map.find(cf_name);
+  cf_handle = (it != m_cf_name_map.end()) ? it->second : nullptr;
+
+  if (!cf_handle) {
+    LogPluginErrMsg(WARNING_LEVEL, 0, "Column family '%s' not found.",
+                    cf_name.c_str());
+  }
+
+  RDB_MUTEX_UNLOCK_CHECK(m_mutex);
+
+  return cf_handle;
+}
+
+rocksdb::ColumnFamilyHandle *Rdb_cf_manager::get_cf(const uint32_t &id) const {
+  rocksdb::ColumnFamilyHandle *cf_handle = nullptr;
+
+  RDB_MUTEX_LOCK_CHECK(m_mutex);
+  const auto it = m_cf_id_map.find(id);
+  if (it != m_cf_id_map.end())
+    cf_handle = it->second;
+  RDB_MUTEX_UNLOCK_CHECK(m_mutex);
+
+  return cf_handle;
+}
+
+std::vector<std::string> Rdb_cf_manager::get_cf_names(void) const {
+  std::vector<std::string> names;
+
+  RDB_MUTEX_LOCK_CHECK(m_mutex);
+  for (auto it : m_cf_name_map) {
+    names.push_back(it.first);
+  }
+  RDB_MUTEX_UNLOCK_CHECK(m_mutex);
+
+  return names;
+}
+
+std::vector<rocksdb::ColumnFamilyHandle *>
+Rdb_cf_manager::get_all_cf(void) const {
+  std::vector<rocksdb::ColumnFamilyHandle *> list;
+
+  RDB_MUTEX_LOCK_CHECK(m_mutex);
+
+  for (auto it : m_cf_id_map) {
+    assert(it.second != nullptr);
+    list.push_back(it.second);
+  }
+
+  RDB_MUTEX_UNLOCK_CHECK(m_mutex);
+
+  return list;
+}
+
+}  // namespace myrocks

storage/rocksdb/rdb_perf_context.h
@@ -1,0 +1,151 @@
+/*
+   Portions Copyright (c) 2015-Present, Facebook, Inc.
+   Portions Copyright (c) 2012,2013 Monty Program Ab
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; version 2 of the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
+#pragma once
+
+/* C++ standard header files */
+#include <atomic>
+#include <cstdint>
+#include <string>
+
+/* MySQL header files */
+#include "my_io_perf.h"
+#include "sql/handler.h"
+
+namespace myrocks {
+
+enum {
+  PC_USER_KEY_COMPARISON_COUNT = 0,
+  PC_BLOCK_CACHE_HIT_COUNT,
+  PC_BLOCK_READ_COUNT,
+  PC_BLOCK_READ_BYTE,
+  PC_BLOCK_READ_TIME,
+  PC_BLOCK_CHECKSUM_TIME,
+  PC_BLOCK_DECOMPRESS_TIME,
+  PC_GET_READ_BYTES,
+  PC_MULTIGET_READ_BYTES,
+  PC_ITER_READ_BYTES,
+  PC_KEY_SKIPPED,
+  PC_DELETE_SKIPPED,
+  PC_RECENT_SKIPPED,
+  PC_MERGE,
+  PC_GET_SNAPSHOT_TIME,
+  PC_GET_FROM_MEMTABLE_TIME,
+  PC_GET_FROM_MEMTABLE_COUNT,
+  PC_GET_POST_PROCESS_TIME,
+  PC_GET_FROM_OUTPUT_FILES_TIME,
+  PC_SEEK_ON_MEMTABLE_TIME,
+  PC_SEEK_ON_MEMTABLE_COUNT,
+  PC_NEXT_ON_MEMTABLE_COUNT,
+  PC_PREV_ON_MEMTABLE_COUNT,
+  PC_SEEK_CHILD_SEEK_TIME,
+  PC_SEEK_CHILD_SEEK_COUNT,
+  PC_SEEK_MIN_HEAP_TIME,
+  PC_SEEK_MAX_HEAP_TIME,
+  PC_SEEK_INTERNAL_SEEK_TIME,
+  PC_FIND_NEXT_USER_ENTRY_TIME,
+  PC_WRITE_WAL_TIME,
+  PC_WRITE_MEMTABLE_TIME,
+  PC_WRITE_DELAY_TIME,
+  PC_WRITE_PRE_AND_POST_PROCESSS_TIME,
+  PC_DB_MUTEX_LOCK_NANOS,
+  PC_DB_CONDITION_WAIT_NANOS,
+  PC_MERGE_OPERATOR_TIME_NANOS,
+  PC_READ_INDEX_BLOCK_NANOS,
+  PC_READ_FILTER_BLOCK_NANOS,
+  PC_NEW_TABLE_BLOCK_ITER_NANOS,
+  PC_NEW_TABLE_ITERATOR_NANOS,
+  PC_BLOCK_SEEK_NANOS,
+  PC_FIND_TABLE_NANOS,
+  PC_BLOOM_MEMTABLE_HIT_COUNT,
+  PC_BLOOM_MEMTABLE_MISS_COUNT,
+  PC_BLOOM_SST_HIT_COUNT,
+  PC_BLOOM_SST_MISS_COUNT,
+  PC_KEY_LOCK_WAIT_TIME,
+  PC_KEY_LOCK_WAIT_COUNT,
+  PC_IO_THREAD_POOL_ID,
+  PC_IO_BYTES_WRITTEN,
+  PC_IO_BYTES_READ,
+  PC_IO_OPEN_NANOS,
+  PC_IO_ALLOCATE_NANOS,
+  PC_IO_WRITE_NANOS,
+  PC_IO_READ_NANOS,
+  PC_IO_RANGE_SYNC_NANOS,
+  PC_IO_LOGGER_NANOS,
+  PC_MAX_IDX
+};
+
+class Rdb_perf_counters;
+
+/*
+  A collection of performance counters that can be safely incremented by
+  multiple threads since it stores atomic datapoints.
+*/
+struct Rdb_atomic_perf_counters {
+  std::atomic_ullong m_value[PC_MAX_IDX];
+};
+
+/*
+  A collection of performance counters that is meant to be incremented by
+  a single thread.
+*/
+class Rdb_perf_counters {
+  Rdb_perf_counters(const Rdb_perf_counters &) = delete;
+  Rdb_perf_counters &operator=(const Rdb_perf_counters &) = delete;
+
+ public:
+  Rdb_perf_counters() = default;
+  uint64_t m_value[PC_MAX_IDX];
+
+  void load(const Rdb_atomic_perf_counters &atomic_counters);
+};
+
+extern std::string rdb_pc_stat_types[PC_MAX_IDX];
+
+/*
+  Perf timers for data reads
+ */
+class Rdb_io_perf {
+  // Context management
+  Rdb_atomic_perf_counters *m_atomic_counters = nullptr;
+  my_io_perf_atomic_t *m_shared_io_perf_read = nullptr;
+  ha_statistics *m_stats = nullptr;
+
+ public:
+  Rdb_io_perf(const Rdb_io_perf &) = delete;
+  Rdb_io_perf &operator=(const Rdb_io_perf &) = delete;
+
+  void init(Rdb_atomic_perf_counters *const atomic_counters,
+            my_io_perf_atomic_t *const shared_io_perf_read,
+            ha_statistics *const stats) {
+    assert(atomic_counters != nullptr);
+    assert(shared_io_perf_read != nullptr);
+    assert(stats != nullptr);
+
+    m_atomic_counters = atomic_counters;
+    m_shared_io_perf_read = shared_io_perf_read;
+    m_stats = stats;
+  }
+
+  bool start(const uint32_t perf_context_level);
+  void end_and_record(const uint32_t perf_context_level);
+
+  explicit Rdb_io_perf()
+      : m_atomic_counters(nullptr), m_shared_io_perf_read(nullptr),
+        m_stats(nullptr) {}
+};
+
+}  // namespace myrocks