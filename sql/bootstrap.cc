inikep/percona-server

[userstat] PS-4790: Improve user statistics accuracy
Measure times for `dispatch_command` instead of mysql_parse, mysqld_stmt_reset, mysqld_stmt_fetch, mysqld_stmt_prepare, mysqld_stmt_execute.

sql/bootstrap.cc
@@ -249,7 +249,7 @@ static int process_iterator(THD *thd, Command_iterator *it,
 
     // Ignore ER_TOO_LONG_KEY for system tables.
     thd->push_internal_handler(&error_handler);
-    mysql_parse(thd, &parser_state);
+    mysql_parse(thd, &parser_state, true);
     thd->pop_internal_handler();
 
     error = thd->is_error();

sql/log_event.cc
@@ -4789,7 +4789,7 @@ int Query_log_event::do_apply_event(Relay_log_info const *rli,
           query_start_status = thd->status_var;
         }
 
-        mysql_parse(thd, &parser_state);
+        mysql_parse(thd, &parser_state, true);
 
         enum_sql_command command = thd->lex->sql_command;
 

sql/sql_parse.cc
@@ -167,6 +167,7 @@
 #include "sql/thd_raii.h"
 #include "sql/transaction.h"  // trans_rollback_implicit
 #include "sql/transaction_info.h"
+#include "sql/userstat.h"
 #include "sql_string.h"
 #include "thr_lock.h"
 #include "violite.h"
@@ -1456,7 +1457,7 @@ static void check_secondary_engine_statement(THD *thd,
   thd->variables.option_bits |= OPTION_LOG_OFF;
 
   // Restart the statement.
-  mysql_parse(thd, parser_state);
+  mysql_parse(thd, parser_state, true);
 
   // Restore the original option bits.
   thd->variables.option_bits = saved_option_bits;
@@ -1581,6 +1582,12 @@ bool dispatch_command(THD *thd, const COM_DATA *com_data,
 
   if (!(server_command_flags[command] & CF_SKIP_QUESTIONS))
     thd->status_var.questions++;
+
+  /* Declare userstat variables and start timer */
+  double start_busy_usecs = 0.0;
+  double start_cpu_nsecs = 0.0;
+  if (unlikely(opt_userstat))
+    userstat_start_timer(&start_busy_usecs, &start_cpu_nsecs);
 
   /**
     Clear the set of flags that are expected to be cleared at the
@@ -1803,7 +1810,7 @@ bool dispatch_command(THD *thd, const COM_DATA *com_data,
       thd->set_secondary_engine_optimization(
           Secondary_engine_optimization::PRIMARY_TENTATIVELY);
 
-      mysql_parse(thd, &parser_state);
+      mysql_parse(thd, &parser_state, false);
 
       // Check if the statement failed and needs to be restarted in
       // another storage engine.
@@ -1888,7 +1895,7 @@ bool dispatch_command(THD *thd, const COM_DATA *com_data,
         thd->set_secondary_engine_optimization(
             Secondary_engine_optimization::PRIMARY_TENTATIVELY);
         /* TODO: set thd->lex->sql_command to SQLCOM_END here */
-        mysql_parse(thd, &parser_state);
+        mysql_parse(thd, &parser_state, false);
 
         check_secondary_engine_statement(thd, &parser_state,
                                          beginning_of_next_stmt, length);
@@ -2180,6 +2187,15 @@ bool dispatch_command(THD *thd, const COM_DATA *com_data,
 done:
   DBUG_ASSERT(thd->open_tables == NULL ||
               (thd->locked_tables_mode == LTM_LOCK_TABLES));
+
+  /* Update user statistics only if at least one timer was initialized */
+  if (unlikely(start_busy_usecs > 0.0 || start_cpu_nsecs > 0.0)) {
+    userstat_finish_timer(start_busy_usecs, start_cpu_nsecs, &thd->busy_time,
+                          &thd->cpu_time);
+    /* Updates THD stats and the global user stats. */
+    thd->update_stats(true);
+    update_global_user_stats(thd, true, time(nullptr));
+  }
 
   /* Finalize server status flags after executing a command. */
   thd->update_slow_query_status();
@@ -5328,7 +5344,7 @@ bool create_select_for_variable(Parse_context *pc, const char *var_name) {
   @param parser_state Parser state.
 */
 
-void mysql_parse(THD *thd, Parser_state *parser_state) {
+void mysql_parse(THD *thd, Parser_state *parser_state, bool update_userstat) {
   DBUG_TRACE;
   DBUG_PRINT("mysql_parse", ("query: '%s'", thd->query().str));
 
@@ -5337,31 +5353,11 @@ void mysql_parse(THD *thd, Parser_state *parser_state) {
   mysql_reset_thd_for_next_command(thd);
   lex_start(thd);
 
-  int start_time_error = 0;
-  int end_time_error = 0;
-  struct timeval start_time, end_time;
-  double start_usecs = 0;
-  double end_usecs = 0;
-  /* cpu time */
-  int cputime_error = 0;
-#ifdef HAVE_CLOCK_GETTIME
-  struct timespec tp;
-#endif
-  double start_cpu_nsecs = 0;
-  double end_cpu_nsecs = 0;
-
-  if (opt_userstat) {
-#ifdef HAVE_CLOCK_GETTIME
-    /* get start cputime */
-    if (!(cputime_error = clock_gettime(CLOCK_THREAD_CPUTIME_ID, &tp)))
-      start_cpu_nsecs = tp.tv_sec * 1000000000.0 + tp.tv_nsec;
-#endif
-
-    // Gets the start time, in order to measure how long this command takes.
-    if (!(start_time_error = gettimeofday(&start_time, nullptr))) {
-      start_usecs = start_time.tv_sec * 1000000.0 + start_time.tv_usec;
-    }
-  }
+  /* Declare userstat variables and start timer */
+  double start_busy_usecs = 0.0;
+  double start_cpu_nsecs = 0.0;
+  if (unlikely(opt_userstat && update_userstat))
+    userstat_start_timer(&start_busy_usecs, &start_cpu_nsecs);
 
   thd->m_parser_state = parser_state;
   invoke_pre_parse_rewrite_plugins(thd);
@@ -5520,45 +5516,14 @@ void mysql_parse(THD *thd, Parser_state *parser_state) {
   thd->cleanup_after_query();
   DBUG_ASSERT(thd->change_list.is_empty());
 
-  if (opt_userstat) {
-    // Gets the end time.
-    if (!(end_time_error = gettimeofday(&end_time, NULL))) {
-      end_usecs = end_time.tv_sec * 1000000.0 + end_time.tv_usec;
-    }
-
-    // Calculates the difference between the end and start times.
-    if (start_usecs && end_usecs >= start_usecs && !start_time_error &&
-        !end_time_error) {
-      thd->busy_time = (end_usecs - start_usecs) / 1000000;
-      // In case there are bad values, 2629743 is the #seconds in a month.
-      if (thd->busy_time > 2629743) {
-        thd->busy_time = 0;
-      }
-    } else {
-      // end time went back in time, or gettimeofday() failed.
-      thd->busy_time = 0;
-    }
-
-#ifdef HAVE_CLOCK_GETTIME
-    /* get end cputime */
-    if (!cputime_error &&
-        !(cputime_error = clock_gettime(CLOCK_THREAD_CPUTIME_ID, &tp)))
-      end_cpu_nsecs = tp.tv_sec * 1000000000.0 + tp.tv_nsec;
-#endif
-    if (start_cpu_nsecs && !cputime_error) {
-      thd->cpu_time = (end_cpu_nsecs - start_cpu_nsecs) / 1000000000;
-      // In case there are bad values, 2629743 is the #seconds in a month.
-      if (thd->cpu_time > 2629743) {
-        thd->cpu_time = 0;
-      }
-    } else
-      thd->cpu_time = 0;
-  }
-
-  // Updates THD stats and the global user stats.
-  if (unlikely(opt_userstat)) {
+  /* Update user statistics only if at least one timer was initialized */
+  if (unlikely(update_userstat &&
+               (start_busy_usecs > 0.0 || start_cpu_nsecs > 0.0))) {
+    userstat_finish_timer(start_busy_usecs, start_cpu_nsecs, &thd->busy_time,
+                          &thd->cpu_time);
+    /* Updates THD stats and the global user stats. */
     thd->update_stats(true);
-    update_global_user_stats(thd, true, time(NULL));
+    update_global_user_stats(thd, true, time(nullptr));
   }
 
   DEBUG_SYNC(thd, "query_rewritten");

mydb-ru/mydb-server

sql/bootstrap.cc
@@ -251,7 +251,7 @@ static int process_iterator(THD *thd, Command_iterator *it,
 
     // Ignore ER_TOO_LONG_KEY for system tables.
     thd->push_internal_handler(&error_handler);
-    dispatch_sql_command(thd, &parser_state);
+    dispatch_sql_command(thd, &parser_state, true);
     thd->pop_internal_handler();
 
     error = thd->is_error();

sql/log_event.cc
@@ -4833,7 +4833,7 @@ int Query_log_event::do_apply_event(Relay_log_info const *rli,
           thd->copy_status_var(&query_start_status);
         }
 
-        dispatch_sql_command(thd, &parser_state);
+        dispatch_sql_command(thd, &parser_state, true);
 
         enum_sql_command command = thd->lex->sql_command;
 

sql/sql_parse.cc
@@ -178,6 +178,7 @@
 #include "sql/thd_raii.h"
 #include "sql/transaction.h"  // trans_rollback_implicit
 #include "sql/transaction_info.h"
+#include "sql/userstat.h"
 #include "sql_string.h"
 #include "template_utils.h"
 #include "thr_lock.h"
@@ -1606,7 +1607,7 @@ static void check_secondary_engine_statement(THD *thd,
   thd->variables.option_bits |= OPTION_LOG_OFF;
 
   // Restart the statement.
-  dispatch_sql_command(thd, parser_state);
+  dispatch_sql_command(thd, parser_state, true);
 
   // Restore the original option bits.
   thd->variables.option_bits = saved_option_bits;
@@ -1801,6 +1802,12 @@ bool dispatch_command(THD *thd, const COM_DATA *com_data,
 
   if (!(server_command_flags[command] & CF_SKIP_QUESTIONS))
     thd->status_var.questions++;
+
+  /* Declare userstat variables and start timer */
+  double start_busy_usecs = 0.0;
+  double start_cpu_nsecs = 0.0;
+  if (unlikely(opt_userstat))
+    userstat_start_timer(&start_busy_usecs, &start_cpu_nsecs);
 
   /**
     Clear the set of flags that are expected to be cleared at the
@@ -2082,7 +2089,7 @@ bool dispatch_command(THD *thd, const COM_DATA *com_data,
       copy_bind_parameter_values(thd, com_data->com_query.parameters,
                                  com_data->com_query.parameter_count);
 
-      dispatch_sql_command(thd, &parser_state);
+      dispatch_sql_command(thd, &parser_state, false);
 
       // Check if the statement failed and needs to be restarted in
       // another storage engine.
@@ -2169,7 +2176,7 @@ bool dispatch_command(THD *thd, const COM_DATA *com_data,
         thd->set_secondary_engine_optimization(
             Secondary_engine_optimization::PRIMARY_TENTATIVELY);
         /* TODO: set thd->lex->sql_command to SQLCOM_END here */
-        dispatch_sql_command(thd, &parser_state);
+        dispatch_sql_command(thd, &parser_state, false);
 
         check_secondary_engine_statement(thd, &parser_state,
                                          beginning_of_next_stmt, length);
@@ -2478,6 +2485,15 @@ bool dispatch_command(THD *thd, const COM_DATA *com_data,
 done:
   assert(thd->open_tables == nullptr ||
          (thd->locked_tables_mode == LTM_LOCK_TABLES));
+
+  /* Update user statistics only if at least one timer was initialized */
+  if (unlikely(start_busy_usecs > 0.0 || start_cpu_nsecs > 0.0)) {
+    userstat_finish_timer(start_busy_usecs, start_cpu_nsecs, &thd->busy_time,
+                          &thd->cpu_time);
+    /* Updates THD stats and the global user stats. */
+    thd->update_stats(true);
+    update_global_user_stats(thd, true, time(nullptr));
+  }
 
   /* Finalize server status flags after executing a command. */
   thd->update_slow_query_status();
@@ -5370,7 +5386,8 @@ void statement_id_to_session(THD *thd) {
   @param parser_state Parser state.
 */
 
-void dispatch_sql_command(THD *thd, Parser_state *parser_state) {
+void dispatch_sql_command(THD *thd, Parser_state *parser_state,
+                          bool update_userstat) {
   DBUG_TRACE;
   DBUG_PRINT("dispatch_sql_command", ("query: '%s'", thd->query().str));
   statement_id_to_session(thd);
@@ -5382,31 +5399,11 @@ void dispatch_sql_command(THD *thd, Parser_state *parser_state) {
   thd->reset_rewritten_query();
   lex_start(thd);
 
-  int start_time_error = 0;
-  int end_time_error = 0;
-  struct timeval start_time, end_time;
-  double start_usecs = 0;
-  double end_usecs = 0;
-  /* cpu time */
-  int cputime_error = 0;
-#ifdef HAVE_CLOCK_GETTIME
-  struct timespec tp;
-#endif
-  double start_cpu_nsecs = 0;
-  double end_cpu_nsecs = 0;
-
-  if (opt_userstat) {
-#ifdef HAVE_CLOCK_GETTIME
-    /* get start cputime */
-    if (!(cputime_error = clock_gettime(CLOCK_THREAD_CPUTIME_ID, &tp)))
-      start_cpu_nsecs = tp.tv_sec * 1000000000.0 + tp.tv_nsec;
-#endif
-
-    // Gets the start time, in order to measure how long this command takes.
-    if (!(start_time_error = gettimeofday(&start_time, nullptr))) {
-      start_usecs = start_time.tv_sec * 1000000.0 + start_time.tv_usec;
-    }
-  }
+  /* Declare userstat variables and start timer */
+  double start_busy_usecs = 0.0;
+  double start_cpu_nsecs = 0.0;
+  if (unlikely(opt_userstat && update_userstat))
+    userstat_start_timer(&start_busy_usecs, &start_cpu_nsecs);
 
   thd->m_parser_state = parser_state;
   invoke_pre_parse_rewrite_plugins(thd);
@@ -5576,45 +5573,14 @@ void dispatch_sql_command(THD *thd, Parser_state *parser_state) {
   thd->cleanup_after_query();
   assert(thd->change_list.is_empty());
 
-  if (opt_userstat) {
-    // Gets the end time.
-    if (!(end_time_error = gettimeofday(&end_time, NULL))) {
-      end_usecs = end_time.tv_sec * 1000000.0 + end_time.tv_usec;
-    }
-
-    // Calculates the difference between the end and start times.
-    if (start_usecs && end_usecs >= start_usecs && !start_time_error &&
-        !end_time_error) {
-      thd->busy_time = (end_usecs - start_usecs) / 1000000;
-      // In case there are bad values, 2629743 is the #seconds in a month.
-      if (thd->busy_time > 2629743) {
-        thd->busy_time = 0;
-      }
-    } else {
-      // end time went back in time, or gettimeofday() failed.
-      thd->busy_time = 0;
-    }
-
-#ifdef HAVE_CLOCK_GETTIME
-    /* get end cputime */
-    if (!cputime_error &&
-        !(cputime_error = clock_gettime(CLOCK_THREAD_CPUTIME_ID, &tp)))
-      end_cpu_nsecs = tp.tv_sec * 1000000000.0 + tp.tv_nsec;
-#endif
-    if (start_cpu_nsecs && !cputime_error) {
-      thd->cpu_time = (end_cpu_nsecs - start_cpu_nsecs) / 1000000000;
-      // In case there are bad values, 2629743 is the #seconds in a month.
-      if (thd->cpu_time > 2629743) {
-        thd->cpu_time = 0;
-      }
-    } else
-      thd->cpu_time = 0;
-  }
-
-  // Updates THD stats and the global user stats.
-  if (unlikely(opt_userstat)) {
+  /* Update user statistics only if at least one timer was initialized */
+  if (unlikely(update_userstat &&
+               (start_busy_usecs > 0.0 || start_cpu_nsecs > 0.0))) {
+    userstat_finish_timer(start_busy_usecs, start_cpu_nsecs, &thd->busy_time,
+                          &thd->cpu_time);
+    /* Updates THD stats and the global user stats. */
     thd->update_stats(true);
-    update_global_user_stats(thd, true, time(NULL));
+    update_global_user_stats(thd, true, time(nullptr));
   }
 
   DEBUG_SYNC(thd, "query_rewritten");