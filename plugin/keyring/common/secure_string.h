inikep/percona-server

[plugin/keyring_vault] PS-5364 feature: Update Hashicorp Vault plugin to use v2 API
https://jira.percona.com/browse/PS-5364

Initial implementation from Robert.

PS-5364 feature: Update Hashicorp Vault plugin to use v2 API (MTR combinations)

https://jira.percona.com/browse/PS-5364

Reworked 'keyring_vault' MTR test cases.

Introduced combinations to test different flavors of Vault Server
configurations -'(mount|directory)-(v1|v1|auto_v1|auto_v2|empty_v1|empty_v2)'.
* 'mount' means that different server instances will be using different
  Hashicorp Vault Server mounts. For instance, '/server1' for server1 and
 '/server2' for server2.
* 'directory' means that different server instances will be using different
  path suffixes (directories) under the same Hashicorp Vault Server mount.
  For instance, '/common/server_1' for server1 and '/common/server_2' for
  server2 where '/common' is a shared mount point.
* 'v1' means that all mount points being created will be of type 'kv'
  (version 1) and configuration files will have explicit
  'secret_mount_point_version = 1' parameter.
* 'v2' means that all mount points being created will be of type 'kv-v2'
  (version 2) and configuration files will have explicit
  'secret_mount_point_version = 2' parameter.
* 'auto_v1' means that all mount points being created will be of type 'kv'
  (version 1) and configuration files will have explicit
  'secret_mount_point_version = AUTO' parameter.
* 'auto_v2' means that all mount points being created will be of type 'kv-v2'
  (version 2) and configuration files will have explicit
  'secret_mount_point_version = AUTO' parameter.
* 'empty_v1' means that all mount points being created will be of type 'kv'
  (version 1) and configuration files will not have explicit
  'secret_mount_point_version' parameter.
* 'empty_v2' means that all mount points being created will be of type 'kv-v2'
  (version 2) and configuration files will not have explicit
  'secret_mount_point_version' parameter.

MTR co

plugin/keyring/common/secure_string.h
@@ -20,6 +20,8 @@
 #include <sstream>
 #include "keyring_memory.h"
 
+#include <boost/optional/optional_fwd.hpp>
+
 namespace keyring {
 typedef std::basic_string<char, std::char_traits<char>, Secure_allocator<char>>
     Secure_string;
@@ -29,6 +31,11 @@ typedef std::basic_ostringstream<char, std::char_traits<char>,
 typedef std::basic_istringstream<char, std::char_traits<char>,
                                  Secure_allocator<char>>
     Secure_istringstream;
+typedef std::basic_stringstream<char, std::char_traits<char>,
+                                Secure_allocator<char>>
+    Secure_stringstream;
+
+typedef boost::optional<Secure_string> Optional_secure_string;
 }  // namespace keyring
 
 #endif  // MYSQL_KEYRING_SECURE_STRING

unittest/gunit/keyring_vault/vault_test_base.cc
@@ -1,0 +1,103 @@
+/* Copyright (c) 2018, 2021 Percona LLC and/or its affiliates. All rights
+   reserved.
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License
+   as published by the Free Software Foundation; version 2 of
+   the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA */
+
+#include "vault_test_base.h"
+
+#include <cstdio>
+
+#include <curl/curl.h>
+
+#include "generate_credential_file.h"
+#include "mock_logger.h"
+#include "vault_environment.h"
+#include "vault_mount.h"
+
+/*static*/ bool Vault_test_base::is_env_configured = false;
+/*static*/ void *Vault_test_base::tc_curl_ = nullptr;
+/*static*/ Vault_test_base::ilogger_ptr Vault_test_base::tc_logger_;
+/*static*/ Vault_test_base::vault_mount_ptr Vault_test_base::tc_vault_mount_;
+
+/*virtual*/ Vault_test_base::~Vault_test_base() {}
+
+/*static*/ void Vault_test_base::SetUpTestCase() {
+  if (!(is_env_configured = is_vault_environment_configured())) {
+    return;
+  }
+
+  system_charset_info = &my_charset_utf8_general_ci;
+
+  ASSERT_FALSE(generate_credential_file(
+      get_vault_env()->get_default_conf_file_name(),
+      get_vault_env()->get_mount_point_path(),
+      mount_point_version_type::mount_point_version_v1,
+      credentials_validity_type::credentials_validity_correct))
+      << "Could not generate credential file";
+
+  tc_curl_ = curl_easy_init();
+  ASSERT_TRUE(tc_curl_ != nullptr) << "Could not initialize CURL session";
+
+  tc_logger_.reset(new keyring::Mock_logger);
+  ASSERT_TRUE(tc_logger_ != nullptr)
+      << "Could not create Test Case Logger object";
+
+  tc_vault_mount_.reset(new keyring::Vault_mount(static_cast<CURL *>(tc_curl_),
+                                                 tc_logger_.get()));
+  ASSERT_TRUE(tc_vault_mount_ != nullptr)
+      << "Could not create Vault_mount object";
+
+  ASSERT_FALSE(
+      tc_vault_mount_->init(get_vault_env()->get_default_conf_file_name(),
+                            get_vault_env()->get_mount_point_path(),
+                            get_vault_env()->get_admin_token()))
+      << "Could not initialized Vault_mount";
+
+  ASSERT_FALSE(tc_vault_mount_->mount_secret_backend())
+      << "Could not mount secret backend";
+}
+
+/*static*/ void Vault_test_base::TearDownTestCase() {
+  if (!is_env_configured) {
+    return;
+  }
+
+  // remove unique secret mount point
+  ASSERT_FALSE(tc_vault_mount_->unmount_secret_backend())
+      << "Could not unmount secret backend";
+
+  tc_vault_mount_.reset();
+
+  tc_logger_.reset();
+  curl_easy_cleanup(static_cast<CURL *>(tc_curl_));
+  tc_curl_ = nullptr;
+
+  std::remove(get_vault_env()->get_default_conf_file_name().c_str());
+}
+
+keyring::ILogger *Vault_test_base::get_logger() const { return logger_.get(); }
+
+/*virtual*/ void Vault_test_base::SetUp() {
+  logger_.reset(new keyring::Mock_logger);
+  ASSERT_TRUE(logger_ != nullptr) << "Could not create Test Logger object";
+}
+
+/*virtual*/ void Vault_test_base::TearDown() { logger_.reset(); }
+
+Vault_environment *Vault_test_base::get_vault_env() {
+  static auto *env = static_cast<Vault_environment *>(
+      ::testing::AddGlobalTestEnvironment(new Vault_environment()));
+  return env;
+}

mydb-ru/mydb-server

plugin/keyring/common/secure_string.h
@@ -17,7 +17,9 @@
 #ifndef MYSQL_KEYRING_SECURE_STRING
 #define MYSQL_KEYRING_SECURE_STRING
 
+#include <optional>
 #include <sstream>
+
 #include "keyring_memory.h"
 
 namespace keyring {
@@ -29,6 +31,11 @@ typedef std::basic_ostringstream<char, std::char_traits<char>,
 typedef std::basic_istringstream<char, std::char_traits<char>,
                                  Secure_allocator<char>>
     Secure_istringstream;
+typedef std::basic_stringstream<char, std::char_traits<char>,
+                                Secure_allocator<char>>
+    Secure_stringstream;
+
+typedef std::optional<Secure_string> Optional_secure_string;
 }  // namespace keyring
 
 #endif  // MYSQL_KEYRING_SECURE_STRING

unittest/gunit/keyring_vault/vault_test_base.cc
@@ -1,0 +1,103 @@
+/* Copyright (c) 2018, 2021 Percona LLC and/or its affiliates. All rights
+   reserved.
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License
+   as published by the Free Software Foundation; version 2 of
+   the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA */
+
+#include "vault_test_base.h"
+
+#include <cstdio>
+
+#include <curl/curl.h>
+
+#include "generate_credential_file.h"
+#include "mock_logger.h"
+#include "vault_environment.h"
+#include "vault_mount.h"
+
+/*static*/ bool Vault_test_base::is_env_configured = false;
+/*static*/ void *Vault_test_base::tc_curl_ = nullptr;
+/*static*/ Vault_test_base::ilogger_ptr Vault_test_base::tc_logger_;
+/*static*/ Vault_test_base::vault_mount_ptr Vault_test_base::tc_vault_mount_;
+
+/*virtual*/ Vault_test_base::~Vault_test_base() {}
+
+/*static*/ void Vault_test_base::SetUpTestCase() {
+  if (!(is_env_configured = is_vault_environment_configured())) {
+    return;
+  }
+
+  system_charset_info = &my_charset_utf8mb3_general_ci;
+
+  ASSERT_FALSE(generate_credential_file(
+      get_vault_env()->get_default_conf_file_name(),
+      get_vault_env()->get_mount_point_path(),
+      mount_point_version_type::mount_point_version_v1,
+      credentials_validity_type::credentials_validity_correct))
+      << "Could not generate credential file";
+
+  tc_curl_ = curl_easy_init();
+  ASSERT_TRUE(tc_curl_ != nullptr) << "Could not initialize CURL session";
+
+  tc_logger_.reset(new keyring::Mock_logger);
+  ASSERT_TRUE(tc_logger_ != nullptr)
+      << "Could not create Test Case Logger object";
+
+  tc_vault_mount_.reset(new keyring::Vault_mount(static_cast<CURL *>(tc_curl_),
+                                                 tc_logger_.get()));
+  ASSERT_TRUE(tc_vault_mount_ != nullptr)
+      << "Could not create Vault_mount object";
+
+  ASSERT_FALSE(
+      tc_vault_mount_->init(get_vault_env()->get_default_conf_file_name(),
+                            get_vault_env()->get_mount_point_path(),
+                            get_vault_env()->get_admin_token()))
+      << "Could not initialized Vault_mount";
+
+  ASSERT_FALSE(tc_vault_mount_->mount_secret_backend())
+      << "Could not mount secret backend";
+}
+
+/*static*/ void Vault_test_base::TearDownTestCase() {
+  if (!is_env_configured) {
+    return;
+  }
+
+  // remove unique secret mount point
+  ASSERT_FALSE(tc_vault_mount_->unmount_secret_backend())
+      << "Could not unmount secret backend";
+
+  tc_vault_mount_.reset();
+
+  tc_logger_.reset();
+  curl_easy_cleanup(static_cast<CURL *>(tc_curl_));
+  tc_curl_ = nullptr;
+
+  std::remove(get_vault_env()->get_default_conf_file_name().c_str());
+}
+
+keyring::ILogger *Vault_test_base::get_logger() const { return logger_.get(); }
+
+/*virtual*/ void Vault_test_base::SetUp() {
+  logger_.reset(new keyring::Mock_logger);
+  ASSERT_TRUE(logger_ != nullptr) << "Could not create Test Logger object";
+}
+
+/*virtual*/ void Vault_test_base::TearDown() { logger_.reset(); }
+
+Vault_environment *Vault_test_base::get_vault_env() {
+  static auto *env = static_cast<Vault_environment *>(
+      ::testing::AddGlobalTestEnvironment(new Vault_environment()));
+  return env;
+}