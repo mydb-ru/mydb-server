inikep/percona-server

[plugin/test_udf_wrappers] PS-7348 feature: Create a set of C++ classes / macros that would simplify creation of new UDFs
https://jira.percona.com/browse/PS-7348

Added a set of helper classes / macros which are intended to simplify creation
of new UDFs.

Now any UDF can be created in 3 simple steps.

1. User needs to include 'mysqlpp/udf_wrappers.hpp' file.

2. User needs to declare a class with the following requirements:
- the class must have a constructor that accepts 'mysqlpp::udf_context'
  by non-const reference.
- the class must have a 'calculate()' method that accepts
  'mysqlpp::udf_context' by const reference and returns one of the following:
  * 'mysqlpp::udf_result_t<STRING_RESULT>' (currently 'ext::optional<std::string>') for 'STRING_RESULT' UDFs
  * 'mysqlpp::udf_result_t<REAL_RESULT>' (currently 'ext::optional<double>') for 'REAL_RESULT' UDFs
  * 'mysqlpp::udf_result_t<INT_RESULT>' (currently 'ext::optional<long long>') for 'INT_RESULT' UDFs
  * 'mysqlpp::udf_result_t<STRING_RESULT>' (currently 'ext::optional<std::string>') for 'DECIMAL_RESULT' UDFs
- the class may have a destructor with necessary cleanup logic.

3. User needs to instantiate a set of c-style functions required UDF subsystem
('f_init()' / 'f()' / 'f_deinit()') via:
- 'DECLARE_STRING_UDF()' for 'STRING_RESULT' UDFs
- 'DECLARE_REAL_UDF()'for 'REAL_RESULT' UDFs
- 'DECLARE_INT_UDF()' for 'INT_RESULT' UDFs
- 'DECLARE_DECIMAL_UDF()' for 'DECIMAL_RESULT' UDFs

For instance,
class blah_impl {
  public:
    blah_impl(mysqlpp::udf_context& ctx) {
      if (ctx.get_number_of_args() != 1) // we expect the number of arguments to be exactly 1
        throw std::invalid_argument(
            "function requires exactly one argument");
      ctx.mark_result_const(false);       // specify whether function always return the same value
      ctx.mark_result_nullable(false);    // specify whether function result may be null
      ctx.mark_arg_nullable(0, false);    // we do not expect the first argument t

include/mysqlpp/common_types.hpp
@@ -1,0 +1,52 @@
+/* Copyright (c) 2020 Percona LLC and/or its affiliates. All rights reserved.
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License
+   as published by the Free Software Foundation; version 2 of
+   the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA */
+
+#ifndef MYSQLPP_COMMON_TYPES_HPP
+#define MYSQLPP_COMMON_TYPES_HPP
+
+#include <cassert>
+
+#include <mysql/udf_registration_types.h>
+
+#include <ext/string_view.hpp>
+
+namespace mysqlpp {
+
+using item_result_type = Item_result;
+inline ext::string_view get_item_result_label(
+    item_result_type item_result) noexcept {
+  switch (item_result) {
+    case INVALID_RESULT:
+      return {"invalid"};
+    case STRING_RESULT:
+      return {"string"};
+    case REAL_RESULT:
+      return {"real"};
+    case INT_RESULT:
+      return {"int"};
+    case ROW_RESULT:
+      return {"row"};
+    case DECIMAL_RESULT:
+      return {"decimal"};
+    default:
+      assert(0);
+  }
+  return {};
+}
+
+}  // namespace mysqlpp
+
+#endif

include/mysqlpp/udf_traits.hpp
@@ -1,0 +1,79 @@
+/* Copyright (c) 2020 Percona LLC and/or its affiliates. All rights reserved.
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License
+   as published by the Free Software Foundation; version 2 of
+   the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA */
+
+#ifndef MYSQLPP_UDF_TRAITS_HPP
+#define MYSQLPP_UDF_TRAITS_HPP
+
+#include <string>
+
+#include <ext/optional.hpp>
+#include <ext/string_view.hpp>
+
+namespace mysqlpp {
+
+class udf_context;
+
+struct empty_mixin {};
+
+template <typename T>
+struct wrapped_t {
+  T mixin;
+};
+
+template <typename MixinType, typename ImplType>
+struct impl_with_mixin : public MixinType {
+  impl_with_mixin(udf_context &ctx) : MixinType{}, impl{ctx} {}
+  ImplType impl;
+};
+
+template <item_result_type ItemResult>
+struct udf_traits;
+
+template <>
+struct udf_traits<STRING_RESULT> {
+  using arg_type = ext::string_view;
+  using result_type = ext::optional<std::string>;
+  using mixin_type = wrapped_t<std::string>;
+};
+template <>
+struct udf_traits<REAL_RESULT> {
+  using arg_type = ext::optional<double>;
+  using result_type = ext::optional<double>;
+  using mixin_type = empty_mixin;
+};
+template <>
+struct udf_traits<INT_RESULT> {
+  using arg_type = ext::optional<long long>;
+  using result_type = ext::optional<long long>;
+  using mixin_type = empty_mixin;
+};
+template <>
+struct udf_traits<DECIMAL_RESULT> {
+  using arg_type = ext::string_view;
+  using result_type = ext::optional<std::string>;
+  using mixin_type = wrapped_t<std::string>;
+};
+
+template <item_result_type ItemResult>
+using udf_arg_t = typename udf_traits<ItemResult>::arg_type;
+template <item_result_type ItemResult>
+using udf_result_t = typename udf_traits<ItemResult>::result_type;
+template <item_result_type ItemResult>
+using udf_mixin_t = typename udf_traits<ItemResult>::mixin_type;
+
+}  // namespace mysqlpp
+
+#endif

mydb-ru/mydb-server

include/mysqlpp/common_types.hpp
@@ -1,0 +1,51 @@
+/* Copyright (c) 2020 Percona LLC and/or its affiliates. All rights reserved.
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License
+   as published by the Free Software Foundation; version 2 of
+   the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA */
+
+#ifndef MYSQLPP_COMMON_TYPES_HPP
+#define MYSQLPP_COMMON_TYPES_HPP
+
+#include <cassert>
+#include <string_view>
+
+#include <mysql/udf_registration_types.h>
+
+namespace mysqlpp {
+
+using item_result_type = Item_result;
+inline std::string_view get_item_result_label(
+    item_result_type item_result) noexcept {
+  switch (item_result) {
+    case INVALID_RESULT:
+      return {"invalid"};
+    case STRING_RESULT:
+      return {"string"};
+    case REAL_RESULT:
+      return {"real"};
+    case INT_RESULT:
+      return {"int"};
+    case ROW_RESULT:
+      return {"row"};
+    case DECIMAL_RESULT:
+      return {"decimal"};
+    default:
+      assert(0);
+  }
+  return {};
+}
+
+}  // namespace mysqlpp
+
+#endif

include/mysqlpp/udf_traits.hpp
@@ -1,0 +1,78 @@
+/* Copyright (c) 2020 Percona LLC and/or its affiliates. All rights reserved.
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License
+   as published by the Free Software Foundation; version 2 of
+   the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA */
+
+#ifndef MYSQLPP_UDF_TRAITS_HPP
+#define MYSQLPP_UDF_TRAITS_HPP
+
+#include <optional>
+#include <string>
+#include <string_view>
+
+namespace mysqlpp {
+
+class udf_context;
+
+struct empty_mixin {};
+
+template <typename T>
+struct wrapped_t {
+  T mixin;
+};
+
+template <typename MixinType, typename ImplType>
+struct impl_with_mixin : public MixinType {
+  impl_with_mixin(udf_context &ctx) : MixinType{}, impl{ctx} {}
+  ImplType impl;
+};
+
+template <item_result_type ItemResult>
+struct udf_traits;
+
+template <>
+struct udf_traits<STRING_RESULT> {
+  using arg_type = std::string_view;
+  using result_type = std::optional<std::string>;
+  using mixin_type = wrapped_t<std::string>;
+};
+template <>
+struct udf_traits<REAL_RESULT> {
+  using arg_type = std::optional<double>;
+  using result_type = std::optional<double>;
+  using mixin_type = empty_mixin;
+};
+template <>
+struct udf_traits<INT_RESULT> {
+  using arg_type = std::optional<long long>;
+  using result_type = std::optional<long long>;
+  using mixin_type = empty_mixin;
+};
+template <>
+struct udf_traits<DECIMAL_RESULT> {
+  using arg_type = std::string_view;
+  using result_type = std::optional<std::string>;
+  using mixin_type = wrapped_t<std::string>;
+};
+
+template <item_result_type ItemResult>
+using udf_arg_t = typename udf_traits<ItemResult>::arg_type;
+template <item_result_type ItemResult>
+using udf_result_t = typename udf_traits<ItemResult>::result_type;
+template <item_result_type ItemResult>
+using udf_mixin_t = typename udf_traits<ItemResult>::mixin_type;
+
+}  // namespace mysqlpp
+
+#endif