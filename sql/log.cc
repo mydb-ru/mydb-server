percona/percona-server

PS-7046: Report tables used by query in slow_log
https://jira.percona.com/browse/PS-7046

Add separate field named Query_tables into a slow query log
which contains a comma separated list of tables used by a query.
The log_slow_verbosity should contain 'query_info' for this new
field to be added to slow query log record.

sql/log.cc
@@ -895,6 +895,25 @@ bool File_query_log::write_slow(THD *thd, ulonglong current_utime,
       goto err;
   }
 
+  if ((thd->variables.log_slow_verbosity & (1ULL << SLOG_V_QUERY_INFO))) {
+    // Query_tables
+    std::string tbl_list_str = "";
+    if (thd->lex->query_tables != nullptr) {
+      std::stringstream tbl_list;
+      for (TABLE_LIST *table = thd->lex->query_tables; table;
+           table = table->next_global) {
+        tbl_list << table->get_table_name() << ",";
+      }
+      tbl_list_str = tbl_list.str();
+      tbl_list_str.pop_back();
+    }
+
+    if (my_b_printf(&log_file, "# Query_tables: %s\n", tbl_list_str.c_str()) ==
+        (uint)-1) {
+      goto err;
+    }
+  }
+
   if (thd->db().str && strcmp(thd->db().str, db)) {  // Database changed
     if (my_b_printf(&log_file, "use %s;\n", thd->db().str) == (uint)-1)
       goto err;

mydb-ru/mydb-server

sql/log.cc
@@ -913,6 +913,25 @@ bool File_query_log::write_slow(THD *thd, ulonglong current_utime,
       goto err;
   }
 
+  if ((thd->variables.log_slow_verbosity & (1ULL << SLOG_V_QUERY_INFO))) {
+    // Query_tables
+    std::string tbl_list_str = "";
+    if (thd->lex->query_tables != nullptr) {
+      std::stringstream tbl_list;
+      for (Table_ref *table = thd->lex->query_tables; table;
+           table = table->next_global) {
+        tbl_list << table->get_table_name() << ",";
+      }
+      tbl_list_str = tbl_list.str();
+      tbl_list_str.pop_back();
+    }
+
+    if (my_b_printf(&log_file, "# Query_tables: %s\n", tbl_list_str.c_str()) ==
+        (uint)-1) {
+      goto err;
+    }
+  }
+
   if (thd->db().str && strcmp(thd->db().str, db)) {  // Database changed
     if (my_b_printf(&log_file, "use %s;\n", thd->db().str) == (uint)-1)
       goto err;