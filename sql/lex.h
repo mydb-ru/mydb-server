inikep/percona-server

PS-281: Convert LP 1354988 fix to new "SHOW EFFECTIVE GRANTS"

sql/lex.h
@@ -220,6 +220,7 @@ static const SYMBOL symbols[] = {
     {SYM("DUPLICATE", DUPLICATE_SYM)},
     {SYM("DYNAMIC", DYNAMIC_SYM)},
     {SYM("EACH", EACH_SYM)},
+    {SYM("EFFECTIVE", EFFECTIVE_SYM)},
     {SYM("ELSE", ELSE)},
     {SYM("ELSEIF", ELSEIF_SYM)},
     {SYM("EMPTY", EMPTY_SYM)},

mydb-ru/mydb-server

sql/lex.h
@@ -228,6 +228,7 @@ static const SYMBOL symbols[] = {
     {SYM("DUPLICATE", DUPLICATE_SYM)},
     {SYM("DYNAMIC", DYNAMIC_SYM)},
     {SYM("EACH", EACH_SYM)},
+    {SYM_PERCONA("EFFECTIVE", EFFECTIVE_SYM)},
     {SYM("ELSE", ELSE)},
     {SYM("ELSEIF", ELSEIF_SYM)},
     {SYM("EMPTY", EMPTY_SYM)},