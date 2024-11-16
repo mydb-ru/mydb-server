inikep/percona-server

[strings] PS-269: Initial Percona Server 8.0.12 tree

strings/ctype-utf8.cc
@@ -5246,7 +5246,7 @@ static inline size_t my_strnxfrm_unicode_tmpl(const CHARSET_INFO *cs,
   uchar *dst0 = dst;
   uchar *de = dst + dstlen;
   const uchar *se = src + srclen;
-  DBUG_ASSERT(src);
+  DBUG_ASSERT(src || srclen == 0);
 
   // We manually hoist this if test out of the loop; seemingly GCC
   // (at least 6.1.1) isn't smart enough to do it on its own.
@@ -5350,7 +5350,7 @@ size_t my_strnxfrm_unicode_full_bin(const CHARSET_INFO *cs, uchar *dst,
   uchar *de = dst + dstlen;
   const uchar *se = src + srclen;
 
-  DBUG_ASSERT(src);
+  DBUG_ASSERT(src || srclen == 0);
   DBUG_ASSERT(cs->state & MY_CS_BINSORT);
 
   for (; dst < de && nweights; nweights--) {

mydb-ru/mydb-server

strings/ctype-utf8.cc
@@ -5029,7 +5029,7 @@ static inline size_t my_strnxfrm_unicode_tmpl(const CHARSET_INFO *cs,
   uint8_t *dst0 = dst;
   uint8_t *de = dst + dstlen;
   const uint8_t *se = src + srclen;
-  assert(src);
+  assert(src || srclen == 0);
 
   // We manually hoist this if test out of the loop; seemingly GCC
   // (at least 6.1.1) isn't smart enough to do it on its own.
@@ -5133,7 +5133,7 @@ size_t my_strnxfrm_unicode_full_bin(const CHARSET_INFO *cs, uint8_t *dst,
   uint8_t *de = dst + dstlen;
   const uint8_t *se = src + srclen;
 
-  assert(src);
+  assert(src || srclen == 0);
   assert(cs->state & MY_CS_BINSORT);
 
   for (; dst < de && nweights; nweights--) {