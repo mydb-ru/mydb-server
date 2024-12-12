planetscale/mysql-server

Bug#36879147  [InnoDB] FULLTEXT index limits FTS_DOC_ID to max unsigned 32-bit value
Issue:
FTS_DOC_ID is a 64-bit field and can have values 2^32 and higher. However
current implementation only supports 32-bit value range. This limtation
takes the form of:
 - Assertions
 - Use of unsigned long type which resolves to 32-bit on some platforms
 - VLC (variable length coding) implementation supporting up to 35 buts

Fix:
Support 64-bit doc IDs:
 - Remove assertions
 - Replace use of unsigned long for doc ID deltas with uint64_t
 - Extend VLC functions to support full unsigned 64-bit range

Change-Id: Id96ae8b5d906bda8587bc99f5dd9e9d4fe9375dc

storage/innobase/handler/i_s.cc
@@ -2642,13 +2642,15 @@ static int i_s_fts_index_cache_fill_one_index(
       ptr = node->ilist;
 
       while (decoded < node->ilist_size) {
-        ulint pos = fts_decode_vlc(&ptr);
+        const uint64_t delta = fts_decode_vlc(&ptr);
 
-        doc_id += pos;
+        doc_id += delta;
 
         /* Get position info */
         while (*ptr) {
-          pos = fts_decode_vlc(&ptr);
+          const auto decoded_pos = fts_decode_vlc(&ptr);
+          ut_ad(decoded_pos <= std::numeric_limits<ulint>::max());
+          const auto pos = static_cast<ulint>(decoded_pos);
 
           OK(field_store_string(fields[I_S_FTS_WORD], word_str));
 
@@ -2986,13 +2988,15 @@ static int i_s_fts_index_table_fill_one_fetch(
       ptr = node->ilist;
 
       while (decoded < node->ilist_size) {
-        ulint pos = fts_decode_vlc(&ptr);
+        const uint64_t delta = fts_decode_vlc(&ptr);
 
-        doc_id += pos;
+        doc_id += delta;
 
         /* Get position info */
         while (*ptr) {
-          pos = fts_decode_vlc(&ptr);
+          const auto decoded_pos = fts_decode_vlc(&ptr);
+          ut_ad(decoded_pos <= std::numeric_limits<ulint>::max());
+          const auto pos = static_cast<ulint>(decoded_pos);
 
           OK(field_store_string(fields[I_S_FTS_WORD], word_str));
 

mydb-ru/mydb-server

Backport to 5.7.49.
Change-Id: Ifb56b33c5ec75e578391612eb371c41fc6aeef31

storage/innobase/handler/i_s.cc
@@ -1,6 +1,6 @@
 /*****************************************************************************
 
-Copyright (c) 2007, 2023, Oracle and/or its affiliates.
+Copyright (c) 2007, 2024, Oracle and/or its affiliates.
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License, version 2.0,
@@ -3377,13 +3377,15 @@ i_s_fts_index_cache_fill_one_index(
 			ptr = node->ilist;
 
 			while (decoded < node->ilist_size) {
-				ulint	pos = fts_decode_vlc(&ptr);
+				const uint64_t  delta = fts_decode_vlc(&ptr);
 
-				doc_id += pos;
+				doc_id += delta;
 
 				/* Get position info */
 				while (*ptr) {
-					pos = fts_decode_vlc(&ptr);
+                                        const uint64_t decoded_pos = fts_decode_vlc(&ptr);
+                                        ut_ad(decoded_pos <= std::numeric_limits<ulint>::max());
+                                        const ulint pos = static_cast<ulint>(decoded_pos);
 
 					OK(field_store_string(
 						   fields[I_S_FTS_WORD],
@@ -3758,13 +3760,15 @@ i_s_fts_index_table_fill_one_fetch(
 			ptr = node->ilist;
 
 			while (decoded < node->ilist_size) {
-				ulint	pos = fts_decode_vlc(&ptr);
+				const uint64_t  delta = fts_decode_vlc(&ptr);
 
-				doc_id += pos;
+				doc_id += delta;
 
 				/* Get position info */
 				while (*ptr) {
-					pos = fts_decode_vlc(&ptr);
+                                        const uint64_t decoded_pos = fts_decode_vlc(&ptr);
+                                        ut_ad(decoded_pos <= std::numeric_limits<ulint>::max());
+                                        const ulint pos = static_cast<ulint>(decoded_pos);
 
 					OK(field_store_string(
 						   fields[I_S_FTS_WORD],