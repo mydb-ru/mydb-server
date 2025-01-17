percona/percona-server

Fixed PS-7778 (Wrongly handled definer for triggers when PS used)
https://jira.percona.com/browse/PS-7778

Problem:
Say user A is set as a definer of the trigger T, and T's body accesses
something that only A has access to. Then, if user B tries to execute a
statement that triggers T, the body of T is executed with the permissions
of user B. An error occurs, since B cannot access something only A is
allowed to access. This error is not the expected behavior: trigger's body
should execute in the security context of user A.
This is caused by the code in Sql_cmd_dml::check_all_table_privileges()
which checks all tables in thd->lex->query_tables list in the security
context of a current user. At the moment the checking occurs, that list
contains not only tables from the executed statement, but all the tables
that need to be touched. That includes tables from attached triggers.
This only happens when a statement is executed as a prepared statement.
Otherwise checks happen in a different order, and this issue is not
exposed.

Fix:
Since all tables in thd->lex->query_tables that come from the executed
statement (own tables) are at the beginning of the list, it's sufficient
to stop checking once all statement's own tables are inspected. Security
checks for triggers happen later when triggers are executed, so no
additional checks for the remainder of thd->lex->query_tables are
necessary.

sql/sql_select.cc
@@ -929,9 +929,10 @@ bool Sql_cmd_select::check_privileges(THD *thd) {
 
 bool Sql_cmd_dml::check_all_table_privileges(THD *thd) {
   // Check for all possible DML privileges
+  TABLE_LIST *first_not_own_table = lex->first_not_own_table();
 
-  for (TABLE_LIST *tr = lex->query_tables; tr != nullptr;
-       tr = tr->next_global) {
+  for (TABLE_LIST *tr = lex->query_tables;
+       tr != nullptr && tr != first_not_own_table; tr = tr->next_global) {
     if (tr->is_internal())  // No privilege check required for internal tables
       continue;
     // Calculated wanted privilege based on how table/view is used:

mydb-ru/mydb-server

sql/sql_select.cc
@@ -1269,10 +1269,10 @@ bool Sql_cmd_select::check_privileges(THD *thd) {
 
 bool Sql_cmd_dml::check_all_table_privileges(THD *thd) {
   // Check for all possible DML privileges
-
   const Table_ref *const first_not_own_table = thd->lex->first_not_own_table();
-  for (Table_ref *tr = lex->query_tables; tr != first_not_own_table;
-       tr = tr->next_global) {
+
+  for (Table_ref *tr = lex->query_tables;
+       tr != nullptr && tr != first_not_own_table; tr = tr->next_global) {
     if (tr->is_internal())  // No privilege check required for internal tables
       continue;
     // Calculated wanted privilege based on how table/view is used: