3992ccdf447f3a61d36e3a4ae48d9b6453ae2083
Alexey Kopytov <akopytov@mydb.ru>
2025-01-24 20:34:36 +0300

    Restart MTR with failing tests only
    Give unstable tests (or those incompatible with --mem) a chance by
    restarting MTR with a list of tests that failed on the first run.

--- .gitflic/debug.sh
+++ .gitflic/debug.sh
@@ -3,8 +3,6 @@
 set -xeuo pipefail
 
 build() {
-    rm -rf /build/*
-
     # To be able to generate commit ID in INFO_SRC
     git config --global --add safe.directory /src
 
@@ -30,10 +28,36 @@ test() {
     # Have to run MTR as non-root. Changing stdin to /dev/null is a workaround
     # for a weird main.loaddata_special failure that only occurs when starting
     # MTR under non-root user in a Docker container
-    su - ubuntu <<EOF
-/build/mysql-test/mtr --mem -parallel=auto \
+    {
+        su - ubuntu <<EOF
+/build/mysql-test/mtr --mem --parallel=auto \
     --suite=default --force --max-test-fail=0 --report-unstable-tests \
     --unit-tests --unit-tests-report < /dev/null
+EOF
+    } || true
+
+    local failures
+
+    set +x
+    failures="$(grep 'Failing test(s): ' /build/debug_log.txt)"
+    set -x
+
+    failures="${failures/Failing test(s): /}"
+    failures="${failures/shutdown_report/}"
+    failures="${failures/unit_tests/--unit-tests --unit-tests-report}"
+
+    if [[ -z "$failures" ]]; then
+        return 0
+    fi
+
+    rm -rf /build/mysql-test/var
+
+    # Re-run failing tests without /dev/shm but with libeatmydata
+    su - ubuntu <<EOF
+eatmydata /build/mysql-test/mtr --parallel=auto \
+    --suite=default --force --max-test-fail=0 --report-unstable-tests \
+    $failures \
+    < /dev/null
 EOF
 }
 
