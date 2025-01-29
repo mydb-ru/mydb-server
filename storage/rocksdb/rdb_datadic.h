facebook/mysql-5.6

PS-7027: MyRocks: Add SK index-only scans for all remaining collations
https://jira.percona.com/browse/PS-7027

1. Index-only scans with SK are supported by decoding the memcomparable form back into the original form.
Currently we support SK index-only scans for:
- `binary`, `utf8_bin`, and `utf8mb4_bin` collations
- collations based on `my_collation_8bit_simple_ci_handler` (e.g. `latin1_swedish_ci`, `cp850_general_ci`, `latin1_german1_ci`, `cp1251_ukrainian_ci`, `cp866_general_ci`)
- collations based on `my_collation_8bit_bin_handler` (i.e. `latinX_bin`, `cp12XX_bin`, `cp85X_bin`, `armscii8_bin`, `ascii_bin` and many more)

This patch turns on support for all remaining collations by storing entire copy of original field in the value.
The downside is that extra storage is used for the index.

2. Add backward compatibility with PRIMARY_FORMAT_VERSION_COLL and SECONDARY_FORMAT_VERSION_COLL
after introducing a support for index-only scans with SK for all previously-unsupported collations
by storing entire copy of original field in `unpack_info`.

storage/rocksdb/rdb_datadic.h
@@ -391,6 +391,13 @@ class Rdb_key_def {
            m_kv_format_version >= SECONDARY_FORMAT_VERSION_UPDATE4;
   }
 
+  inline bool force_SK_index_only_scans() const {
+    return (m_index_type == INDEX_TYPE_SECONDARY &&
+            m_kv_format_version >= SECONDARY_FORMAT_VERSION_COLL) ||
+           (m_index_type == INDEX_TYPE_PRIMARY &&
+            m_kv_format_version >= PRIMARY_FORMAT_VERSION_COLL);
+  }
+
   /* Indicates that all key parts can be unpacked to cover a secondary lookup */
   bool can_cover_lookup() const;
 
@@ -574,7 +581,10 @@ class Rdb_key_def {
     //  - Add a lead segment byte in space padding varchar datatypes to
     //    optimize for empty content.
     PRIMARY_FORMAT_VERSION_UPDATE3 = 14,
-    PRIMARY_FORMAT_VERSION_LATEST = PRIMARY_FORMAT_VERSION_UPDATE3,
+    // This change includes:
+    //  - NO_PAD collations should not use the space padded encoding
+    PRIMARY_FORMAT_VERSION_COLL = 15,
+    PRIMARY_FORMAT_VERSION_LATEST = PRIMARY_FORMAT_VERSION_COLL,
 
     SECONDARY_FORMAT_VERSION_INITIAL = 10,
     // This change the SK format to include unpack_info.
@@ -596,7 +606,11 @@ class Rdb_key_def {
     // 2-byte bitmap is added after the tag-byte to unpack_info only for
     // records which have covered varchar/blob columns.
     SECONDARY_FORMAT_VERSION_UPDATE4 = 15,
-    SECONDARY_FORMAT_VERSION_LATEST = SECONDARY_FORMAT_VERSION_UPDATE4,
+    // This change includes support for index-only scans with SK for all
+    // previously-unsupported collations by storing entire copy of original
+    // field in "unpack_info".
+    SECONDARY_FORMAT_VERSION_COLL = 16,
+    SECONDARY_FORMAT_VERSION_LATEST = SECONDARY_FORMAT_VERSION_COLL,
   };
 
   // Stores if the secondary index keys are covered for index scans or not.
@@ -1821,8 +1835,5 @@ class Rdb_system_merge_op : public rocksdb::AssociativeMergeOperator {
     return rdb_netbuf_to_uint16(reinterpret_cast<const uchar *>(s.data()));
   }
 };
-
-bool rdb_is_simple_collation(const my_core::CHARSET_INFO *const cs);
-bool rdb_is_binary_collation(const my_core::CHARSET_INFO *const cs);
 
 }  // namespace myrocks

mydb-ru/mydb-server

Upstream commit ID : fb-mysql-5.6.35/41d164795d6b8c1353f186520d45ff5ff5956d63
PS-7310 : Merge fb-prod8-202008
Summary:
1. Index-only scans with SK are supported by decoding the memcomparable
form back into the original form.  Currently we support SK index-only
scans for:
- collations based on `my_collation_8bit_simple_ci_handler` (e.g.
  `latin1_swedish_ci`, `cp850_general_ci`, `latin1_german1_ci`,
  `cp1251_ukrainian_ci`, `cp866_general_ci`)
- collations based on `my_collation_8bit_bin_handler` (i.e.
  `latinX_bin`, `cp12XX_bin`, `cp85X_bin`, `armscii8_bin`, `ascii_bin`
  and many more)
This patch turns on support for all remaining collations by storing
entire copy of original field in the value.
2. Add backward compatibility with PRIMARY_FORMAT_VERSION_COLL and
SECONDARY_FORMAT_VERSION_COLL after introducing a support for index-only
scans with SK for all previously-unsupported collations by storing
entire copy of original field in `unpack_info`.
Porting Note: Patch 5/5 for PS-7027 MyRocks
Reviewed By: Pushapgl
Differential Revision: D22899844
fbshipit-source-id: a305f2743b8

storage/rocksdb/rdb_datadic.h
@@ -401,6 +401,13 @@ class Rdb_key_def {
            m_index_type == INDEX_TYPE_HIDDEN_PRIMARY;
   }
 
+  inline bool supports_index_only_collation_scans() const {
+    return (m_index_type == INDEX_TYPE_SECONDARY &&
+            m_kv_format_version >= SECONDARY_FORMAT_VERSION_COLL) ||
+           (m_index_type == INDEX_TYPE_PRIMARY &&
+            m_kv_format_version >= PRIMARY_FORMAT_VERSION_COLL);
+  }
+
   /* Indicates that all key parts can be unpacked to cover a secondary lookup */
   bool can_cover_lookup() const;
 
@@ -584,7 +591,10 @@ class Rdb_key_def {
     //  - Add a lead segment byte in space padding varchar datatypes to
     //    optimize for empty content.
     PRIMARY_FORMAT_VERSION_UPDATE3 = 14,
-    PRIMARY_FORMAT_VERSION_LATEST = PRIMARY_FORMAT_VERSION_UPDATE3,
+    // This change includes:
+    //  - NO_PAD collations should not use the space padded encoding
+    PRIMARY_FORMAT_VERSION_COLL = 15,
+    PRIMARY_FORMAT_VERSION_LATEST = PRIMARY_FORMAT_VERSION_COLL,
 
     SECONDARY_FORMAT_VERSION_INITIAL = 10,
     // This change the SK format to include unpack_info.
@@ -606,7 +616,11 @@ class Rdb_key_def {
     // 2-byte bitmap is added after the tag-byte to unpack_info only for
     // records which have covered varchar/blob columns.
     SECONDARY_FORMAT_VERSION_UPDATE4 = 15,
-    SECONDARY_FORMAT_VERSION_LATEST = SECONDARY_FORMAT_VERSION_UPDATE4,
+    // This change includes support for index-only scans with SK for all
+    // previously-unsupported collations by storing entire copy of original
+    // field in "unpack_info".
+    SECONDARY_FORMAT_VERSION_COLL = 16,
+    SECONDARY_FORMAT_VERSION_LATEST = SECONDARY_FORMAT_VERSION_COLL,
   };
 
   // Stores if the secondary index keys are covered for index scans or not.
@@ -1844,8 +1858,5 @@ class Rdb_system_merge_op : public rocksdb::AssociativeMergeOperator {
     return rdb_netbuf_to_uint16(reinterpret_cast<const uchar *>(s.data()));
   }
 };
-
-bool rdb_is_simple_collation(const my_core::CHARSET_INFO *const cs);
-bool rdb_is_binary_collation(const my_core::CHARSET_INFO *const cs);
 
 }  // namespace myrocks