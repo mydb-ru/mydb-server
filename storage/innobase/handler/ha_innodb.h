percona/percona-server

PS-5783: New thread variables: innodb_(force_index_)records_in_range
These variables make it possible to override reconds_in_range, effectively
skipping its computation. This could be useful in some cases where reconds_in_range
takes up most of the query cost.

storage/innobase/handler/ha_innodb.h
@@ -1197,3 +1197,6 @@ innobase_create_zip_dict_references(
 /** Free InnoDB session specific data.
 @param[in,out]	thd	MySQL thread handler. */
 void thd_free_innodb_session(THD* thd);
+
+uint innodb_force_index_records_in_range(THD* thd);
+uint innodb_records_in_range(THD* thd);

mydb-ru/mydb-server

storage/innobase/handler/ha_innodb.h
@@ -1404,6 +1404,9 @@ bool innobase_build_index_translation(const TABLE *table,
                                       dict_table_t *ib_table,
                                       INNOBASE_SHARE *share);
 
+uint innodb_force_index_records_in_range(THD *thd);
+uint innodb_records_in_range(THD *thd);
+
 /** Free InnoDB session specific data.
 @param[in,out]	thd	MySQL thread handler. */
 void thd_free_innodb_session(THD *thd) noexcept;