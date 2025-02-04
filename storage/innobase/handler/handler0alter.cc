inikep/percona-server

[encryption] PS-269: temp_tablespace_encrypt
PS-5363 (Merge MySQL 8.0.17): revert temp file encryption

and MariaDB IOCACHE refactoring in preparation for taking in 8.0.17
binlog cache encryption. Remove parts of percona_encrypt_tmp_files
testcase that deal with binlog cache encryption.

storage/innobase/handler/handler0alter.cc
@@ -5239,6 +5239,7 @@ bool ha_innobase::prepare_inplace_alter_table_impl(
   bool add_fts_idx = false;
   dict_s_col_list *s_cols = nullptr;
   mem_heap_t *s_heap = nullptr;
+  ulint encrypt_flag = 0;
 
   DBUG_TRACE;
   DBUG_ASSERT(!ha_alter_info->handler_ctx);
@@ -5410,6 +5411,26 @@ bool ha_innobase::prepare_inplace_alter_table_impl(
 
   if (!info.innobase_table_flags()) {
     goto err_exit_no_heap;
+  }
+
+  /* create_table_info_t::innobase_table_flags does not set encryption
+  flags. There are places where it is done afterwards, there are places
+  where it isn't done. We need to inspect all code paths and check if
+  encryption flag can be set in one place. */
+  if (!Encryption::is_none(ha_alter_info->create_info->encrypt_type.str)) {
+    /* Set the encryption flag. */
+    byte *master_key = nullptr;
+    ulint master_key_id;
+
+    /* Check if keyring is ready. */
+    Encryption::get_master_key(&master_key_id, &master_key);
+
+    if (master_key == nullptr) {
+      goto err_exit_no_heap;
+    } else {
+      my_free(master_key);
+      encrypt_flag = DICT_TF2_ENCRYPTION_FILE_PER_TABLE;
+    }
   }
 
   max_col_len = DICT_MAX_FIELD_LEN_BY_FORMAT_FLAG(info.flags());
@@ -5856,7 +5877,7 @@ bool ha_innobase::prepare_inplace_alter_table_impl(
 
   return prepare_inplace_alter_table_dict(
       ha_alter_info, altered_table, table, old_dd_tab, new_dd_tab,
-      table_share->table_name.str, info.flags(), info.flags2(),
+      table_share->table_name.str, info.flags(), info.flags2() | encrypt_flag,
       fts_doc_col_no, add_fts_doc_id, add_fts_doc_id_idx, m_prebuilt);
 }
 

storage/innobase/include/row0log.h
@@ -214,6 +214,37 @@ of an ALTER TABLE for this index.
 ulint row_log_estimate_work(const dict_index_t *index);
 #endif /* HAVE_PSI_STAGE_INTERFACE */
 
+/** Find out if temporary log files encrypted.
+@return true if temporary log file should be encrypted, false if not */
+MY_NODISCARD
+bool log_tmp_is_encrypted() noexcept;
+
+/** Check the row log encryption is enabled or not.
+It will enable the row log encryption. */
+void log_tmp_enable_encryption_if_set();
+
+/** Encrypt a temporary file block.
+@param[in]	src		block to encrypt
+@param[in]	size		size of the block
+@param[out]	dst		destination block
+@param[in]	offs		offset to block
+@param[in]	space_id	tablespace id
+@return whether the operation succeeded */
+MY_NODISCARD
+bool log_tmp_block_encrypt(const byte *src_block, ulint size, byte *dst_block,
+                           os_offset_t offs, space_id_t space_id);
+
+/** Decrypt a temporary file block.
+@param[in]	src		block to decrypt
+@param[in]	size		size of the block
+@param[out]	dst		destination block
+@param[in]	offs		offset to block
+@param[in]	space_id	tablespace id
+@return whether the operation succeeded */
+MY_NODISCARD
+bool log_tmp_block_decrypt(const byte *src_block, ulint size, byte *dst_block,
+                           os_offset_t offs, space_id_t space_id);
+
 #include "row0log.ic"
 
 #endif /* row0log.h */

mydb-ru/mydb-server

storage/innobase/handler/handler0alter.cc
@@ -5478,6 +5478,7 @@ bool ha_innobase::prepare_inplace_alter_table_impl(
   bool add_fts_idx = false;
   dict_s_col_list *s_cols = nullptr;
   mem_heap_t *s_heap = nullptr;
+  ulint encrypt_flag = 0;
 
   DBUG_TRACE;
   assert(!ha_alter_info->handler_ctx);
@@ -5623,6 +5624,26 @@ bool ha_innobase::prepare_inplace_alter_table_impl(
 
   if (!info.innobase_table_flags()) {
     goto err_exit_no_heap;
+  }
+
+  /* create_table_info_t::innobase_table_flags does not set encryption
+  flags. There are places where it is done afterwards, there are places
+  where it isn't done. We need to inspect all code paths and check if
+  encryption flag can be set in one place. */
+  if (!Encryption::is_none(ha_alter_info->create_info->encrypt_type.str)) {
+    /* Set the encryption flag. */
+    byte *master_key = nullptr;
+    uint32_t master_key_id;
+
+    /* Check if keyring is ready. */
+    Encryption::get_master_key(&master_key_id, &master_key);
+
+    if (master_key == nullptr) {
+      goto err_exit_no_heap;
+    } else {
+      my_free(master_key);
+      encrypt_flag = DICT_TF2_ENCRYPTION_FILE_PER_TABLE;
+    }
   }
 
   max_col_len = DICT_MAX_FIELD_LEN_BY_FORMAT_FLAG(info.flags());
@@ -6076,7 +6097,7 @@ bool ha_innobase::prepare_inplace_alter_table_impl(
 
   return prepare_inplace_alter_table_dict(
       ha_alter_info, altered_table, table, old_dd_tab, new_dd_tab,
-      table_share->table_name.str, info.flags(), info.flags2(),
+      table_share->table_name.str, info.flags(), info.flags2() | encrypt_flag,
       fts_doc_col_no, add_fts_doc_id, add_fts_doc_id_idx, m_prebuilt);
 }
 

storage/innobase/include/row0log.h
@@ -210,6 +210,37 @@ of an ALTER TABLE for this index.
 ulint row_log_estimate_work(const dict_index_t *index);
 #endif /* HAVE_PSI_STAGE_INTERFACE */
 
+/** Find out if temporary log files encrypted.
+@return true if temporary log file should be encrypted, false if not */
+[[nodiscard]]
+bool log_tmp_is_encrypted() noexcept;
+
+/** Check the row log encryption is enabled or not.
+It will enable the row log encryption. */
+void log_tmp_enable_encryption_if_set();
+
+/** Encrypt a temporary file block.
+@param[in]	src_block	block to encrypt
+@param[in]	size		size of the block
+@param[out]	dst_block	destination block
+@param[in]	offs		offset to block
+@param[in]	space_id	tablespace id
+@return whether the operation succeeded */
+[[nodiscard]]
+bool log_tmp_block_encrypt(const byte *src_block, ulint size, byte *dst_block,
+                           os_offset_t offs, space_id_t space_id);
+
+/** Decrypt a temporary file block.
+@param[in]	src_block	block to decrypt
+@param[in]	size		size of the block
+@param[out]	dst_block	destination block
+@param[in]	offs		offset to block
+@param[in]	space_id	tablespace id
+@return whether the operation succeeded */
+[[nodiscard]]
+bool log_tmp_block_decrypt(const byte *src_block, ulint size, byte *dst_block,
+                           os_offset_t offs, space_id_t space_id);
+
 #include "row0log.ic"
 
 #endif /* row0log.h */