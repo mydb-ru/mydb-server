percona/percona-server

Fixed PS-7873 (log_status table reports wrong gtid_executed) https://jira.percona.com/browse/PS-7873
Problem
A commit has 3 stages:
1. Flushing - protected by LOCK_log
2. Syncing- we release LOCK_log and acquire LOCK_sync
3. Committing - we release LOCK_sync and acquire LOCK_commit. In this
stage we also acquire global_sid_lock and update gtid_executed.

When querying log_status table, we acquire LOCK_log for binlog at
Log_resource_binlog_wrapper::lock() and global_sid_lock for
gtid_executed at Log_resource_gtid_state_wrapper::lock().

In high level, Gtid works as follow:
1. a THD ask for the next GTID no for the server ID it's executing the
transaction.
2. it generates the binlog event with said GTID.
3. at stage 3 if THD owns any gtid, it updates gtid_executed

The problem comes when a query to ps.log_status comes after stage 1 of
binlog commit. At this stage, commit has released LOCK_log, binlg file
and position are already updated, Log_resource_binlog_wrapper::lock()
will move forward. Then while binlog is still processing stage 2,
Log_resource_gtid_state_wrapper::lock() will be invoked and will acquire
global_sid_lock. At this point stage 3 of commit will not be able to
update gtid_executed leaving the log_status query with a mismatch
between binlog position (new values) vs gtid_executed (old values).

Fix:
Increase the lock at log_status table to also take LOCK_sync and
LOCK_commit if GTID wrapper has been invoked. This way the query to
log_status will have to wait until gtid_executed has been fully updated
guaranteeing consistency with binlog position.

sql/log_resource.cc
@@ -21,6 +21,7 @@
    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA */
 
 #include "sql/log_resource.h"
+#include "debug_sync.h"
 #include "sql/json_dom.h"
 
 int MY_ATTRIBUTE((visibility("default")))
@@ -100,12 +101,23 @@ bool Log_resource_binlog_wrapper::collect_info() {
   return error;
 }
 
-void Log_resource_gtid_state_wrapper::lock() { global_sid_lock->wrlock(); }
+void Log_resource_gtid_state_wrapper::lock() {
+  DEBUG_SYNC(current_thd, "log_resource_gtid_lock");
+  mysql_mutex_lock(binlog->get_sync_lock());
+  mysql_mutex_lock(binlog->get_commit_lock());
+  global_sid_lock->wrlock();
+}
 
-void Log_resource_gtid_state_wrapper::unlock() { global_sid_lock->unlock(); }
+void Log_resource_gtid_state_wrapper::unlock() {
+  global_sid_lock->unlock();
+  mysql_mutex_unlock(binlog->get_commit_lock());
+  mysql_mutex_unlock(binlog->get_sync_lock());
+}
 
 bool Log_resource_gtid_state_wrapper::collect_info() {
   bool error = false;
+  mysql_mutex_assert_owner(binlog->get_sync_lock());
+  mysql_mutex_assert_owner(binlog->get_commit_lock());
   global_sid_lock->assert_some_wrlock();
 
   char *gtid_executed_string;
@@ -150,9 +162,10 @@ Log_resource *Log_resource_factory::get_wrapper(MYSQL_BIN_LOG *binlog,
 }
 
 Log_resource *Log_resource_factory::get_wrapper(Gtid_state *gtid_state,
+                                                MYSQL_BIN_LOG *binlog,
                                                 Json_dom *json) {
   Log_resource_gtid_state_wrapper *wrapper;
-  wrapper = new Log_resource_gtid_state_wrapper(gtid_state, json);
+  wrapper = new Log_resource_gtid_state_wrapper(gtid_state, binlog, json);
   return wrapper;
 }
 

mydb-ru/mydb-server

sql/log_resource.cc
@@ -22,6 +22,7 @@
    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA */
 
 #include "sql/log_resource.h"
+#include "debug_sync.h"
 #include "sql-common/json_dom.h"
 
 int MY_ATTRIBUTE((visibility("default")))
@@ -101,12 +102,23 @@ bool Log_resource_binlog_wrapper::collect_info() {
   return error;
 }
 
-void Log_resource_gtid_state_wrapper::lock() { global_tsid_lock->wrlock(); }
+void Log_resource_gtid_state_wrapper::lock() {
+  DEBUG_SYNC(current_thd, "log_resource_gtid_lock");
+  mysql_mutex_lock(binlog->get_sync_lock());
+  mysql_mutex_lock(binlog->get_commit_lock());
+  global_tsid_lock->wrlock();
+}
 
-void Log_resource_gtid_state_wrapper::unlock() { global_tsid_lock->unlock(); }
+void Log_resource_gtid_state_wrapper::unlock() {
+  global_tsid_lock->unlock();
+  mysql_mutex_unlock(binlog->get_commit_lock());
+  mysql_mutex_unlock(binlog->get_sync_lock());
+}
 
 bool Log_resource_gtid_state_wrapper::collect_info() {
   bool error = false;
+  mysql_mutex_assert_owner(binlog->get_sync_lock());
+  mysql_mutex_assert_owner(binlog->get_commit_lock());
   global_tsid_lock->assert_some_wrlock();
 
   char *gtid_executed_string;
@@ -152,9 +164,10 @@ Log_resource *Log_resource_factory::get_wrapper(MYSQL_BIN_LOG *binlog,
 }
 
 Log_resource *Log_resource_factory::get_wrapper(Gtid_state *gtid_state,
+                                                MYSQL_BIN_LOG *binlog,
                                                 Json_dom *json) {
   Log_resource_gtid_state_wrapper *wrapper;
-  wrapper = new Log_resource_gtid_state_wrapper(gtid_state, json);
+  wrapper = new Log_resource_gtid_state_wrapper(gtid_state, binlog, json);
   return wrapper;
 }
 