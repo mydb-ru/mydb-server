inikep/percona-server

PS-8032 Inplace Add index with lock=exclusive does not generate MLOG_ADD_INDEX redo
https://jira.percona.com/browse/PS-8032

Problem:
Server is not generating MLOG_ADD_INDEX redo in case of inplace add index
with exclusive lock DDL. MLOG_ADD_INDEX tell if any transaction
happened without redo logging. It is very important for backup tool to know if
any transaction happened without redo logging.

Fix:
Generate MLOG_ADD_INDEX if index was created using btr_blk.

storage/innobase/ddl/ddl0builder.cc
@@ -1902,7 +1902,7 @@ dberr_t Builder::fts_sort_and_build() noexcept {
   }
 }
 
-dberr_t Builder::finalize() noexcept {
+dberr_t Builder::finalize(bool apply_log) noexcept {
   ut_a(m_ctx.m_need_observer);
   ut_a(get_state() == State::FINISH);
 
@@ -1924,11 +1924,13 @@ dberr_t Builder::finalize() noexcept {
   if (err == DB_SUCCESS) {
     write_redo(m_index);
 
-    DEBUG_SYNC_C_IF_THD(m_ctx.thd(), "row_log_apply_before");
+    if (apply_log) {
+      DEBUG_SYNC_C_IF_THD(m_ctx.thd(), "row_log_apply_before");
 
-    err = row_log_apply(m_ctx.m_trx, m_index, m_ctx.m_table, m_local_stage);
+      err = row_log_apply(m_ctx.m_trx, m_index, m_ctx.m_table, m_local_stage);
 
-    DEBUG_SYNC_C_IF_THD(m_ctx.thd(), "row_log_apply_after");
+      DEBUG_SYNC_C_IF_THD(m_ctx.thd(), "row_log_apply_after");
+    }
   }
 
   if (err != DB_SUCCESS) {
@@ -2005,19 +2007,17 @@ dberr_t Builder::finish() noexcept {
   }
 
   dberr_t err{DB_SUCCESS};
-
-  if (get_error() != DB_SUCCESS || !m_ctx.m_online) {
+  if (get_error() == DB_SUCCESS && !m_index->table->is_temporary()) {
+    bool apply_log = true;
     /* Do not apply any online log. */
-  } else if (m_ctx.m_old_table != m_ctx.m_new_table) {
-    ut_a(!m_index->online_log);
-    ut_a(m_index->online_status == ONLINE_INDEX_COMPLETE);
-
-    auto observer = m_ctx.m_trx->flush_observer;
-    observer->flush();
-
-  } else {
-    err = finalize();
-
+    if (!m_ctx.m_online) {
+      apply_log = false;
+    } else if (m_ctx.m_old_table != m_ctx.m_new_table) {
+      ut_a(!m_index->online_log);
+      ut_a(m_index->online_status == ONLINE_INDEX_COMPLETE);
+      apply_log = false;
+    }
+    err = finalize(apply_log);
     if (err != DB_SUCCESS) {
       set_error(err);
     }

mydb-ru/mydb-server

storage/innobase/ddl/ddl0builder.cc
@@ -1957,7 +1957,7 @@ dberr_t Builder::fts_sort_and_build() noexcept {
   }
 }
 
-dberr_t Builder::finalize() noexcept {
+dberr_t Builder::finalize(bool apply_log) noexcept {
   ut_a(m_ctx.m_need_observer);
   ut_a(get_state() == State::FINISH);
 
@@ -1979,11 +1979,13 @@ dberr_t Builder::finalize() noexcept {
   if (err == DB_SUCCESS) {
     write_redo(m_index);
 
-    DEBUG_SYNC(m_ctx.thd(), "row_log_apply_before");
+    if (apply_log) {
+      DEBUG_SYNC(m_ctx.thd(), "row_log_apply_before");
 
-    err = row_log_apply(m_ctx.m_trx, m_index, m_ctx.m_table, m_local_stage);
+      err = row_log_apply(m_ctx.m_trx, m_index, m_ctx.m_table, m_local_stage);
 
-    DEBUG_SYNC(m_ctx.thd(), "row_log_apply_after");
+      DEBUG_SYNC(m_ctx.thd(), "row_log_apply_after");
+    }
   }
 
   if (err != DB_SUCCESS) {
@@ -2060,19 +2062,17 @@ dberr_t Builder::finish() noexcept {
   }
 
   dberr_t err{DB_SUCCESS};
-
-  if (get_error() != DB_SUCCESS || !m_ctx.m_online) {
+  if (get_error() == DB_SUCCESS && !m_index->table->is_temporary()) {
+    bool apply_log = true;
     /* Do not apply any online log. */
-  } else if (m_ctx.m_old_table != m_ctx.m_new_table) {
-    ut_a(!m_index->online_log);
-    ut_a(m_index->online_status == ONLINE_INDEX_COMPLETE);
-
-    auto observer = m_ctx.m_trx->flush_observer;
-    observer->flush();
-
-  } else {
-    err = finalize();
-
+    if (!m_ctx.m_online) {
+      apply_log = false;
+    } else if (m_ctx.m_old_table != m_ctx.m_new_table) {
+      ut_a(!m_index->online_log);
+      ut_a(m_index->online_status == ONLINE_INDEX_COMPLETE);
+      apply_log = false;
+    }
+    err = finalize(apply_log);
     if (err != DB_SUCCESS) {
       set_error(err);
     }